48abf5b27e47512f4e77bd02cccf05af77714998
Rémi Bernon <rbernon@codeweavers.com>
2019-08-28 22:24:40 +0200

    ntdll: Add thread destroy notification function.
    This will be used in LFH to recycle the thread local data.

--- dlls/ntdll/loader.c
+++ dlls/ntdll/loader.c
@@ -3550,6 +3550,7 @@ void WINAPI RtlExitUserProcess( DWORD status )
     RtlAcquirePebLock();
     NtTerminateProcess( 0, status );
     LdrShutdownProcess();
+    HEAP_notify_thread_destroy(TRUE);
     for (;;) NtTerminateProcess( GetCurrentProcess(), status );
 }
 
