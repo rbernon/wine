a5b45346ba467376c1be7b7f2483ff85ba5d4926
Rémi Bernon <rbernon@codeweavers.com>
2020-02-12 19:22:50 +0100

    user32/tests: Ignore WM_APP-1 message in win tests.

--- dlls/user32/tests/win.c
+++ dlls/user32/tests/win.c
@@ -3947,7 +3947,8 @@ static BOOL wait_for_message( MSG *msg )
         if (ret)
         {
             if (msg->message == WM_PAINT ||
-                msg->message == WM_IME_NOTIFY)
+                msg->message == WM_IME_NOTIFY ||
+                msg->message == WM_APP-1)
                 DispatchMessageA(msg);
             else break;
         }
