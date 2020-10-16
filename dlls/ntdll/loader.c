a29b8ff9d409354bbc24ef3fb5a91458a4e9b98e
Rémi Bernon <rbernon@codeweavers.com>
2020-10-16 19:31:59 +0200

    ntdll: Add traces to LdrGetProcedureAddress.

--- dlls/ntdll/loader.c
+++ dlls/ntdll/loader.c
@@ -1952,6 +1952,8 @@ NTSTATUS WINAPI LdrGetProcedureAddress(HMODULE module, const ANSI_STRING *name,
     DWORD exp_size;
     NTSTATUS ret = STATUS_PROCEDURE_NOT_FOUND;
 
+    TRACE("module %p, name %s, ord %ld, address %p\n", module, debugstr_a(name ? name->Buffer : NULL), ord, address);
+
     RtlEnterCriticalSection( &loader_section );
 
     /* check if the module itself is invalid to return the proper error */
@@ -1967,6 +1969,8 @@ NTSTATUS WINAPI LdrGetProcedureAddress(HMODULE module, const ANSI_STRING *name,
             ret = STATUS_SUCCESS;
         }
     }
+
+    if (ret) WARN("couldn't find procedure %s (%ld), returning %#lx\n", debugstr_a(name ? name->Buffer : NULL), ord, ret);
 
     RtlLeaveCriticalSection( &loader_section );
     return ret;
