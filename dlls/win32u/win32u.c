4ae747fff2934014e338af5d7123ff3425741358
Rémi Bernon <rbernon@codeweavers.com>
2020-12-10 13:26:10 +0100

    win32u: Introduce new win32u dll.

---
+++ dlls/win32u/win32u.c
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2020 Rémi Bernon for CodeWeavers
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#include <stdarg.h>
+
+#include "windef.h"
+#include "winbase.h"
+#include "winnt.h"
+
+#include "wine/debug.h"
+
+#include "unixlib.h"
+
+WINE_DEFAULT_DEBUG_CHANNEL(win32u);
+
+static struct unix_funcs *unix_funcs;
+
+BOOL WINAPI DllMain( HINSTANCE instance, DWORD reason, LPVOID reserved )
+{
+    TRACE( "instance %p, reason %d, reserved %p.\n", instance, reason, reserved );
+
+    switch (reason)
+    {
+    case DLL_PROCESS_ATTACH: DisableThreadLibraryCalls( instance ); break;
+    case DLL_PROCESS_DETACH: break;
+    }
+
+    if (__wine_init_unix_lib( instance, reason, NULL, &unix_funcs )) return FALSE;
+    return TRUE;
+}
