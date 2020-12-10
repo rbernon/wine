1ae6f45e2ec58d9437426d51f4051977c9675d32
Rémi Bernon <rbernon@codeweavers.com>
2020-12-10 22:58:57 +0100

    include: Add new win32u desktop internal IOCTLs.

---
+++ include/wine/win32u.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2020 Rémi Bernon for CodeWeavers
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#ifndef __WINE_WINE_WIN32U_H
+#define __WINE_WINE_WIN32U_H
+
+#include <windef.h>
+#include <winuser.h>
+#include <winioctl.h>
+
+static inline const char *debugstr_win32u_ioctl( DWORD code )
+{
+    switch (code)
+    {
+    default: return "unknown";
+    }
+}
+
+#endif
