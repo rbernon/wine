12457c14bf86e492aece200d84c3ac59d81ea0de
Rémi Bernon <rbernon@codeweavers.com>
2020-12-10 13:26:10 +0100

    win32k.sys: Introduce new win32k.sys dll.

---
+++ dlls/win32k.sys/unix/x11.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2021 Rémi Bernon for CodeWeavers
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#ifndef __WINE_WIN32K_UNIX_X11_H
+#define __WINE_WIN32K_UNIX_X11_H
+
+#ifndef __WINE_CONFIG_H
+#error You must include config.h to use this header
+#endif
+
+#include <assert.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+
+#ifdef HAVE_X11_XLIB_H
+#include <X11/Xlib.h>
+#endif
+
+#undef Status /* avoid conflict with wintrnl.h */
+typedef int Status;
+
+#include "ntstatus.h"
+#define WIN32_NO_STATUS
+#include "winternl.h"
+#include "winnt.h"
+#include "winbase.h"
+#include "winuser.h"
+#include "wingdi.h"
+
+#include "wine/gdi_driver.h"
+#include "wine/debug.h"
+
+#endif /* __WINE_WIN32K_UNIX_X11_H */
