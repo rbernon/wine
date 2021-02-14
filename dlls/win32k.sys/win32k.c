f7cb22a546eecae3b505b8c9d2a4bc2cdd3e2077
Rémi Bernon <rbernon@codeweavers.com>
2021-02-14 18:37:29 +0100

    win32k.sys: Process desktop IOCTLs during wait.

--- dlls/win32k.sys/win32k.c
+++ dlls/win32k.sys/win32k.c
@@ -18,10 +18,15 @@
 
 #include <stdarg.h>
 
+#include "ntstatus.h"
+#define WIN32_NO_STATUS
 #include "windef.h"
 #include "winbase.h"
 #include "winnt.h"
+#include "winuser.h"
 
+#include "wine/win32u.h"
+#include "wine/server.h"
 #include "wine/debug.h"
 
 #include "unixlib.h"
@@ -29,6 +34,97 @@
 WINE_DEFAULT_DEBUG_CHANNEL(win32k);
 
 static struct unix_funcs *unix_funcs;
+
+static void process_desktop_ioctls(HDESK desktop)
+{
+    char ioctl_buffer[512], *buf = ioctl_buffer;
+    data_size_t out_size = 0, in_size, buf_size = sizeof(ioctl_buffer);
+    unsigned int code;
+    NTSTATUS status = STATUS_SUCCESS;
+
+    for (;;)
+    {
+        if (status) out_size = 0;
+
+        SERVER_START_REQ( get_desktop_ioctl )
+        {
+            req->handle = wine_server_obj_handle( desktop );
+            req->status = status;
+            wine_server_add_data( req, buf, out_size );
+            wine_server_set_reply( req, buf, buf_size );
+            status = wine_server_call( req );
+            code = reply->code;
+            out_size = reply->out_size;
+            in_size = wine_server_reply_size( reply );
+        }
+        SERVER_END_REQ;
+
+        if (out_size > buf_size)
+        {
+            if (buf != ioctl_buffer) buf = HeapReAlloc( GetProcessHeap(), 0, buf, out_size );
+            else
+            {
+                buf = HeapAlloc( GetProcessHeap(), 0, out_size );
+                memcpy( buf, ioctl_buffer, in_size );
+            }
+            if (!buf) status = STATUS_NO_MEMORY;
+            buf_size = out_size;
+        }
+
+        if (status == STATUS_BUFFER_OVERFLOW) continue;
+        if (status) break;
+
+        TRACE( "processing desktop %s ioctl (code=%08x) size %x\n",
+               debugstr_win32u_ioctl( code ), code, in_size );
+
+        out_size = 0;
+        switch (code)
+        {
+        default:
+        {
+            FIXME( "unimplemented desktop %s ioctl (code=%08x) size %x\n",
+                   debugstr_win32u_ioctl( code ), code, in_size );
+            status = STATUS_NOT_IMPLEMENTED;
+            out_size = 0;
+            break;
+        }
+        }
+    }
+
+    if (buf != ioctl_buffer) HeapFree( GetProcessHeap(), 0, buf );
+}
+
+DWORD CDECL win32k_MsgWaitForMultipleObjectsEx( DWORD count, const HANDLE *handles, DWORD timeout,
+                                                DWORD mask, DWORD flags )
+{
+    HANDLE *wait_handles;
+    DWORD res, wait_count = count;
+    HDESK desktop = GetThreadDesktop( GetCurrentThreadId() );
+
+    TRACE( "count %u, handles %p, timeout %u, mask %x, flags %x.\n",
+           count, handles, timeout, mask, flags );
+
+    wait_handles = HeapAlloc( GetProcessHeap(), 0, (count + 1) * sizeof(HANDLE) );
+    if (handles && count) memcpy( wait_handles, handles, count * sizeof(HANDLE) );
+
+    SERVER_START_REQ( get_desktop_wait_handle )
+    {
+        req->handle = wine_server_obj_handle( desktop );
+        if (wine_server_call( req )) WARN( "Unable to get a desktop wait handle!\n" );
+        else wait_handles[wait_count++] = wine_server_ptr_handle( reply->handle );
+    }
+    SERVER_END_REQ;
+
+    do
+    {
+        res = unix_funcs->msg_wait_for_multiple_objects( wait_count, wait_handles, timeout, mask, flags );
+        if (res == WAIT_OBJECT_0 + count) process_desktop_ioctls( desktop );
+    }
+    while (res == WAIT_OBJECT_0 + count);
+
+    HeapFree( GetProcessHeap(), 0, wait_handles );
+    return res;
+}
 
 BOOL WINAPI DllMain( HINSTANCE instance, DWORD reason, LPVOID reserved )
 {
