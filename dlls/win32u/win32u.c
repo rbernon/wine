0635ef4a618d1039afbda252f903b58432078cab
Rémi Bernon <rbernon@codeweavers.com>
2021-02-22 17:26:59 +0100

    WIP: win32u: Hook more gdi driver functions.

--- dlls/win32u/win32u.c
+++ dlls/win32u/win32u.c
@@ -157,6 +157,18 @@ void CDECL win32u_ReleaseDC( HWND hwnd, HDC hdc )
     TRACE( "hwnd %p, hdc %p.\n", hwnd, hdc );
 }
 
+static BOOL CDECL win32u_Arc(PHYSDEV a,INT b,INT c,INT d,INT e,INT f,INT g,INT h,INT i)
+{
+    FIXME("stub!\n");
+    return FALSE;
+}
+
+static BOOL CDECL win32u_Chord(PHYSDEV a,INT b,INT c,INT d,INT e,INT f,INT g,INT h,INT i)
+{
+    FIXME("stub!\n");
+    return FALSE;
+}
+
 static BOOL CDECL win32u_CreateCompatibleDC(PHYSDEV orig, PHYSDEV *dev)
 {
     struct gdi_physdev *phys_dev;
@@ -190,6 +202,18 @@ static BOOL CDECL win32u_DeleteDC(PHYSDEV dev)
     return TRUE;
 }
 
+static BOOL CDECL win32u_Ellipse(PHYSDEV a,INT b,INT c,INT d,INT i)
+{
+    FIXME("stub!\n");
+    return FALSE;
+}
+
+static INT CDECL win32u_EnumICMProfiles(PHYSDEV a,ICMENUMPROCW b,LPARAM i)
+{
+    FIXME("stub!\n");
+    return 0;
+}
+
 static INT CDECL win32u_ExtEscape(PHYSDEV dev, INT escape, INT in_count, const void *in_data,
                                   INT out_count, LPVOID out_data)
 {
@@ -218,14 +242,204 @@ static INT CDECL win32u_ExtEscape(PHYSDEV dev, INT escape, INT in_count, const v
     return 0;
 }
 
+static BOOL CDECL win32u_ExtFloodFill(PHYSDEV a,INT b,INT c,COLORREF d,UINT i)
+{
+    FIXME("stub!\n");
+    return FALSE;
+}
+
+static BOOL CDECL win32u_FillPath(PHYSDEV a)
+{
+    FIXME("stub!\n");
+    return FALSE;
+}
+
+static BOOL CDECL win32u_GetDeviceGammaRamp(PHYSDEV a,LPVOID i)
+{
+    FIXME("stub!\n");
+    return FALSE;
+}
+
+static BOOL CDECL win32u_GetICMProfile(PHYSDEV a,LPDWORD b,LPWSTR i)
+{
+    FIXME("stub!\n");
+    return FALSE;
+}
+
+static DWORD CDECL win32u_GetImage(PHYSDEV a,BITMAPINFO* b,struct gdi_image_bits* c,struct bitblt_coords* i)
+{
+    FIXME("stub!\n");
+    return 0;
+}
+
+static COLORREF CDECL win32u_GetNearestColor(PHYSDEV a,COLORREF i)
+{
+    FIXME("stub!\n");
+    return 0;
+}
+
+static UINT CDECL win32u_GetSystemPaletteEntries(PHYSDEV a,UINT b,UINT c,LPPALETTEENTRY i)
+{
+    FIXME("stub!\n");
+    return 0;
+}
+
+static BOOL CDECL win32u_GradientFill(PHYSDEV a,TRIVERTEX* b,ULONG c,void* d,ULONG e,ULONG i)
+{
+    FIXME("stub!\n");
+    return FALSE;
+}
+
+static BOOL CDECL win32u_LineTo(PHYSDEV a,INT b,INT i)
+{
+    FIXME("stub!\n");
+    return FALSE;
+}
+
+static BOOL CDECL win32u_PaintRgn(PHYSDEV a,HRGN i)
+{
+    FIXME("stub!\n");
+    return FALSE;
+}
+
+static BOOL CDECL win32u_PatBlt(PHYSDEV a,struct bitblt_coords* b,DWORD i)
+{
+    TRACE("stub!\n");
+    return FALSE;
+}
+
+static BOOL CDECL win32u_Pie(PHYSDEV a,INT b,INT c,INT d,INT e,INT f,INT g,INT h,INT i)
+{
+    FIXME("stub!\n");
+    return FALSE;
+}
+
+static BOOL CDECL win32u_PolyPolygon(PHYSDEV a,const POINT* b,const INT* c,UINT i)
+{
+    FIXME("stub!\n");
+    return FALSE;
+}
+
+static BOOL CDECL win32u_PolyPolyline(PHYSDEV a,const POINT* b,const DWORD* c,DWORD i)
+{
+    FIXME("stub!\n");
+    return FALSE;
+}
+
+static BOOL CDECL win32u_Polygon(PHYSDEV a,const POINT* b,INT i)
+{
+    FIXME("stub!\n");
+    return FALSE;
+}
+
+static DWORD CDECL win32u_PutImage(PHYSDEV a,HRGN b,BITMAPINFO* c,const struct gdi_image_bits* d,struct bitblt_coords* e,struct bitblt_coords* f,DWORD i)
+{
+    FIXME("stub!\n");
+    return 0;
+}
+
+static UINT CDECL win32u_RealizeDefaultPalette( PHYSDEV dev )
+{
+    FIXME("stub!\n");
+    return 0;
+}
+
+static UINT CDECL win32u_RealizePalette(PHYSDEV a,HPALETTE b,BOOL i)
+{
+    FIXME("stub!\n");
+    return 0;
+}
+
+static BOOL CDECL win32u_Rectangle(PHYSDEV a,INT b,INT c,INT d,INT i)
+{
+    FIXME("stub!\n");
+    return FALSE;
+}
+
+static BOOL CDECL win32u_RoundRect(PHYSDEV a,INT b,INT c,INT d,INT e,INT f,INT i)
+{
+    FIXME("stub!\n");
+    return FALSE;
+}
+
+static UINT CDECL win32u_SetBoundsRect( PHYSDEV dev, RECT *rect, UINT flags )
+{
+    TRACE("stub!\n");
+    return DCB_RESET;
+}
+
+static COLORREF CDECL win32u_SetDCBrushColor(PHYSDEV a, COLORREF i)
+{
+    FIXME("stub!\n");
+    return 0;
+}
+
+static COLORREF CDECL win32u_SetDCPenColor(PHYSDEV a, COLORREF i)
+{
+    FIXME("stub!\n");
+    return 0;
+}
+
 static void CDECL win32u_SetDeviceClipping( PHYSDEV dev, HRGN rgn )
 {
     TRACE( "dev %p, rgn %p.\n", dev, rgn );
     dev->clip_region = rgn;
 }
 
-extern NTSTATUS CDECL X11DRV_D3DKMTCheckVidPnExclusiveOwnership( const D3DKMT_CHECKVIDPNEXCLUSIVEOWNERSHIP *desc ) DECLSPEC_HIDDEN;
-extern NTSTATUS CDECL X11DRV_D3DKMTSetVidPnSourceOwner( const D3DKMT_SETVIDPNSOURCEOWNER *desc ) DECLSPEC_HIDDEN;
+static BOOL CDECL win32u_SetDeviceGammaRamp(PHYSDEV a,LPVOID i)
+{
+    FIXME("stub!\n");
+    return FALSE;
+}
+
+static COLORREF CDECL win32u_SetPixel(PHYSDEV a,INT b,INT c,COLORREF i)
+{
+    FIXME("stub!\n");
+    return 0;
+}
+
+static BOOL CDECL win32u_StretchBlt(PHYSDEV a,struct bitblt_coords* b,PHYSDEV c,struct bitblt_coords* d,DWORD i)
+{
+    FIXME("stub!\n");
+    return FALSE;
+}
+
+static INT CDECL win32u_StretchDIBits(PHYSDEV dev, INT dst_x, INT dst_y, INT dst_w, INT dst_h, INT src_x, INT src_y, INT src_w, INT src_h, const void *bits, BITMAPINFO *bmi, UINT color_use, DWORD rop)
+{
+    TRACE("dev %p, dst_x %d, dst_y %d, dst_w %d, dst_h %d, src_x %d, src_y %d, src_w %d, src_h %d, bits %p, bmi %p, color_use %u, rop %u\n",
+          dev, dst_x, dst_y, dst_w, dst_h, src_x, src_y, src_w, src_h, bits, bmi, color_use, rop);
+    return 0;
+}
+
+static BOOL CDECL win32u_StrokeAndFillPath(PHYSDEV a)
+{
+    FIXME("stub!\n");
+    return FALSE;
+}
+
+static BOOL CDECL win32u_StrokePath(PHYSDEV a)
+{
+    FIXME("stub!\n");
+    return FALSE;
+}
+
+static BOOL CDECL win32u_UnrealizePalette(HPALETTE i)
+{
+    FIXME("stub!\n");
+    return FALSE;
+}
+
+static NTSTATUS CDECL win32u_D3DKMTCheckVidPnExclusiveOwnership(const D3DKMT_CHECKVIDPNEXCLUSIVEOWNERSHIP * i)
+{
+    FIXME("stub!\n");
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+static NTSTATUS CDECL win32u_D3DKMTSetVidPnSourceOwner(const D3DKMT_SETVIDPNSOURCEOWNER * i)
+{
+    FIXME("stub!\n");
+    return STATUS_NOT_IMPLEMENTED;
+}
 
 struct opengl_funcs * CDECL X11DRV_wine_get_wgl_driver( PHYSDEV dev, UINT version ) DECLSPEC_HIDDEN;
 
@@ -249,30 +463,30 @@ static const struct gdi_dc_funcs gdi_dc_funcs =
     NULL,                               /* pAbortPath */
     NULL,                               /* pAlphaBlend */
     NULL,                               /* pAngleArc */
-    NULL /* X11DRV_Arc */,                         /* pArc */
+    win32u_Arc /* X11DRV_Arc */,                         /* pArc */
     NULL,                               /* pArcTo */
     NULL,                               /* pBeginPath */
     NULL,                               /* pBlendImage */
-    NULL /* X11DRV_Chord */,                       /* pChord */
+    win32u_Chord /* X11DRV_Chord */,                       /* pChord */
     NULL,                               /* pCloseFigure */
     win32u_CreateCompatibleDC /* X11DRV_CreateCompatibleDC */,          /* pCreateCompatibleDC */
     win32u_CreateDC /* X11DRV_CreateDC */,                    /* pCreateDC */
     win32u_DeleteDC /* X11DRV_DeleteDC */,                    /* pDeleteDC */
     NULL,                               /* pDeleteObject */
     NULL,                               /* pDeviceCapabilities */
-    NULL /* X11DRV_Ellipse */,                     /* pEllipse */
+    win32u_Ellipse /* X11DRV_Ellipse */,                     /* pEllipse */
     NULL,                               /* pEndDoc */
     NULL,                               /* pEndPage */
     NULL,                               /* pEndPath */
     NULL,                               /* pEnumFonts */
-    NULL /* X11DRV_EnumICMProfiles */,             /* pEnumICMProfiles */
+    win32u_EnumICMProfiles /* X11DRV_EnumICMProfiles */,             /* pEnumICMProfiles */
     NULL,                               /* pExcludeClipRect */
     NULL,                               /* pExtDeviceMode */
-    win32u_ExtEscape,                   /* pExtEscape */
-    NULL /* X11DRV_ExtFloodFill */,                /* pExtFloodFill */
+    win32u_ExtEscape /* X11DRV_ExtEscape */,                   /* pExtEscape */
+    win32u_ExtFloodFill /* X11DRV_ExtFloodFill */,                /* pExtFloodFill */
     NULL,                               /* pExtSelectClipRgn */
     NULL,                               /* pExtTextOut */
-    NULL /* X11DRV_FillPath */,                    /* pFillPath */
+    win32u_FillPath /* X11DRV_FillPath */,                    /* pFillPath */
     NULL,                               /* pFillRgn */
     NULL,                               /* pFlattenPath */
     NULL,                               /* pFontIsLinked */
@@ -284,51 +498,51 @@ static const struct gdi_dc_funcs gdi_dc_funcs =
     NULL,                               /* pGetCharWidth */
     NULL,                               /* pGetCharWidthInfo */
     NULL /* X11DRV_GetDeviceCaps */,               /* pGetDeviceCaps */
-    NULL /* X11DRV_GetDeviceGammaRamp */,          /* pGetDeviceGammaRamp */
+    win32u_GetDeviceGammaRamp /* X11DRV_GetDeviceGammaRamp */,          /* pGetDeviceGammaRamp */
     NULL,                               /* pGetFontData */
     NULL,                               /* pGetFontRealizationInfo */
     NULL,                               /* pGetFontUnicodeRanges */
     NULL,                               /* pGetGlyphIndices */
     NULL,                               /* pGetGlyphOutline */
-    NULL /* X11DRV_GetICMProfile */,               /* pGetICMProfile */
-    NULL /* X11DRV_GetImage */,                    /* pGetImage */
+    win32u_GetICMProfile /* X11DRV_GetICMProfile */,               /* pGetICMProfile */
+    win32u_GetImage /* X11DRV_GetImage */,                    /* pGetImage */
     NULL,                               /* pGetKerningPairs */
-    NULL /* X11DRV_GetNearestColor */,             /* pGetNearestColor */
+    win32u_GetNearestColor /* X11DRV_GetNearestColor */,             /* pGetNearestColor */
     NULL,                               /* pGetOutlineTextMetrics */
     NULL,                               /* pGetPixel */
-    NULL /* X11DRV_GetSystemPaletteEntries */,     /* pGetSystemPaletteEntries */
+    win32u_GetSystemPaletteEntries /* X11DRV_GetSystemPaletteEntries */,     /* pGetSystemPaletteEntries */
     NULL,                               /* pGetTextCharsetInfo */
     NULL,                               /* pGetTextExtentExPoint */
     NULL,                               /* pGetTextExtentExPointI */
     NULL,                               /* pGetTextFace */
     NULL,                               /* pGetTextMetrics */
-    NULL /* X11DRV_GradientFill */,                /* pGradientFill */
+    win32u_GradientFill /* X11DRV_GradientFill */,                /* pGradientFill */
     NULL,                               /* pIntersectClipRect */
     NULL,                               /* pInvertRgn */
-    NULL /* X11DRV_LineTo */,                      /* pLineTo */
+    win32u_LineTo /* X11DRV_LineTo */,                      /* pLineTo */
     NULL,                               /* pModifyWorldTransform */
     NULL,                               /* pMoveTo */
     NULL,                               /* pOffsetClipRgn */
     NULL,                               /* pOffsetViewportOrg */
     NULL,                               /* pOffsetWindowOrg */
-    NULL /* X11DRV_PaintRgn */,                    /* pPaintRgn */
-    NULL /* X11DRV_PatBlt */,                      /* pPatBlt */
-    NULL /* X11DRV_Pie */,                         /* pPie */
+    win32u_PaintRgn /* X11DRV_PaintRgn */,                    /* pPaintRgn */
+    win32u_PatBlt /* X11DRV_PatBlt */,                      /* pPatBlt */
+    win32u_Pie /* X11DRV_Pie */,                         /* pPie */
     NULL,                               /* pPolyBezier */
     NULL,                               /* pPolyBezierTo */
     NULL,                               /* pPolyDraw */
-    NULL /* X11DRV_PolyPolygon */,                 /* pPolyPolygon */
-    NULL /* X11DRV_PolyPolyline */,                /* pPolyPolyline */
-    NULL /* X11DRV_Polygon */,                     /* pPolygon */
+    win32u_PolyPolygon /* X11DRV_PolyPolygon */,                 /* pPolyPolygon */
+    win32u_PolyPolyline /* X11DRV_PolyPolyline */,                /* pPolyPolyline */
+    win32u_Polygon /* X11DRV_Polygon */,                     /* pPolygon */
     NULL,                               /* pPolyline */
     NULL,                               /* pPolylineTo */
-    NULL /* X11DRV_PutImage */,                    /* pPutImage */
+    win32u_PutImage /* X11DRV_PutImage */,                    /* pPutImage */
     NULL /* X11DRV_RealizeDefaultPalette */,       /* pRealizeDefaultPalette */
-    NULL /* X11DRV_RealizePalette */,              /* pRealizePalette */
-    NULL /* X11DRV_Rectangle */,                   /* pRectangle */
+    win32u_RealizePalette /* X11DRV_RealizePalette */,              /* pRealizePalette */
+    win32u_Rectangle /* X11DRV_Rectangle */,                   /* pRectangle */
     NULL,                               /* pResetDC */
     NULL,                               /* pRestoreDC */
-    NULL /* X11DRV_RoundRect */,                   /* pRoundRect */
+    win32u_RoundRect /* X11DRV_RoundRect */,                   /* pRoundRect */
     NULL,                               /* pSaveDC */
     NULL,                               /* pScaleViewportExt */
     NULL,                               /* pScaleWindowExt */
@@ -341,16 +555,16 @@ static const struct gdi_dc_funcs gdi_dc_funcs =
     NULL,                               /* pSetArcDirection */
     NULL,                               /* pSetBkColor */
     NULL,                               /* pSetBkMode */
-    NULL /* X11DRV_SetBoundsRect */,    /* pSetBoundsRect */
-    NULL /* X11DRV_SetDCBrushColor */,             /* pSetDCBrushColor */
-    NULL /* X11DRV_SetDCPenColor */,               /* pSetDCPenColor */
+    win32u_SetBoundsRect /* X11DRV_SetBoundsRect */,    /* pSetBoundsRect */
+    win32u_SetDCBrushColor /* X11DRV_SetDCBrushColor */,             /* pSetDCBrushColor */
+    win32u_SetDCPenColor /* X11DRV_SetDCPenColor */,               /* pSetDCPenColor */
     NULL,                               /* pSetDIBitsToDevice */
-    win32u_SetDeviceClipping,           /* pSetDeviceClipping */
-    NULL /* X11DRV_SetDeviceGammaRamp */,          /* pSetDeviceGammaRamp */
+    win32u_SetDeviceClipping /* X11DRV_SetDeviceClipping */,/* pSetDeviceClipping */
+    win32u_SetDeviceGammaRamp /* X11DRV_SetDeviceGammaRamp */,          /* pSetDeviceGammaRamp */
     NULL,                               /* pSetLayout */
     NULL,                               /* pSetMapMode */
     NULL,                               /* pSetMapperFlags */
-    NULL /* X11DRV_SetPixel */,                    /* pSetPixel */
+    win32u_SetPixel /* X11DRV_SetPixel */,                    /* pSetPixel */
     NULL,                               /* pSetPolyFillMode */
     NULL,                               /* pSetROP2 */
     NULL,                               /* pSetRelAbs */
@@ -366,14 +580,14 @@ static const struct gdi_dc_funcs gdi_dc_funcs =
     NULL,                               /* pSetWorldTransform */
     NULL,                               /* pStartDoc */
     NULL,                               /* pStartPage */
-    NULL /* X11DRV_StretchBlt */,                  /* pStretchBlt */
+    win32u_StretchBlt /* X11DRV_StretchBlt */,                  /* pStretchBlt */
     NULL,                               /* pStretchDIBits */
-    NULL /* X11DRV_StrokeAndFillPath */,           /* pStrokeAndFillPath */
-    NULL /* X11DRV_StrokePath */,                  /* pStrokePath */
-    NULL /* X11DRV_UnrealizePalette */,            /* pUnrealizePalette */
+    win32u_StrokeAndFillPath /* X11DRV_StrokeAndFillPath */,           /* pStrokeAndFillPath */
+    win32u_StrokePath /* X11DRV_StrokePath */,                  /* pStrokePath */
+    win32u_UnrealizePalette /* X11DRV_UnrealizePalette */,            /* pUnrealizePalette */
     NULL,                               /* pWidenPath */
-    X11DRV_D3DKMTCheckVidPnExclusiveOwnership, /* pD3DKMTCheckVidPnExclusiveOwnership */
-    X11DRV_D3DKMTSetVidPnSourceOwner,   /* pD3DKMTSetVidPnSourceOwner */
+    win32u_D3DKMTCheckVidPnExclusiveOwnership /* X11DRV_D3DKMTCheckVidPnExclusiveOwnership */, /* pD3DKMTCheckVidPnExclusiveOwnership */
+    win32u_D3DKMTSetVidPnSourceOwner /* X11DRV_D3DKMTSetVidPnSourceOwner */,   /* pD3DKMTSetVidPnSourceOwner */
     win32u_wine_get_wgl_driver /* X11DRV_wine_get_wgl_driver */,         /* wine_get_wgl_driver */
     win32u_wine_get_vulkan_driver /* X11DRV_wine_get_vulkan_driver */,      /* wine_get_vulkan_driver */
     GDI_PRIORITY_GRAPHICS_DRV           /* priority */
@@ -382,7 +596,7 @@ static const struct gdi_dc_funcs gdi_dc_funcs =
 /******************************************************************************
  *      win32u_get_gdi_driver
  */
-const struct gdi_dc_funcs *CDECL win32u_get_gdi_driver( unsigned int version )
+const struct gdi_dc_funcs * CDECL win32u_get_gdi_driver( unsigned int version)
 {
     TRACE( "version %u.\n", version );
 
