587d69e28830446c4c9102e725b06f2c79045e5c
Rémi Bernon <rbernon@codeweavers.com>
2021-03-06 20:03:59 +0100

    ntdll: Trace process command-line on startup.

--- dlls/ntdll/loader.c
+++ dlls/ntdll/loader.c
@@ -3752,6 +3752,7 @@ void WINAPI LdrShutdownProcess(void)
  */
 void WINAPI RtlExitUserProcess( DWORD status )
 {
+    ERR( "status %#lx\n", status );
     RtlEnterCriticalSection( &loader_section );
     RtlAcquirePebLock();
     NtTerminateProcess( 0, status );
--- dlls/ntdll/unix/loader.c
+++ dlls/ntdll/unix/loader.c
@@ -2066,7 +2066,7 @@ static ULONG_PTR get_image_address(void)
 /***********************************************************************
  *           start_main_thread
  */
-static void start_main_thread(void)
+static void start_main_thread(int argc, char *argv[])
 {
     SYSTEM_SERVICE_TABLE syscall_table = { (ULONG_PTR *)syscalls, NULL, ARRAY_SIZE(syscalls), syscall_args };
     TEB *teb = virtual_alloc_first_teb();
@@ -2089,6 +2089,8 @@ static void start_main_thread(void)
     if (main_image_info.Machine != current_machine) load_wow64_ntdll( main_image_info.Machine );
     load_apiset_dll();
     ntdll_init_syscalls( 0, &syscall_table, p__wine_syscall_dispatcher );
+    for (int i = 0; i < argc - 1; ++i) MESSAGE(" %s", argv[i]);
+    MESSAGE(" (gdb -p %d)\n", getpid());
     server_init_process_done();
 }
 
@@ -2183,11 +2185,11 @@ static jstring wine_init_jni( JNIEnv *env, jobject obj, jobjectArray cmdline, jo
         __asm__( "mov %%fs,%0" : "=r" (java_fs) );
         if (!(java_fs & 4)) java_gdt_sel = java_fs;
         __asm__( "mov %0,%%fs" :: "r" (0) );
-        start_main_thread();
+        start_main_thread( 0, NULL );
         __asm__( "mov %0,%%fs" :: "r" (java_fs) );
     }
 #else
-    start_main_thread();
+    start_main_thread( 0, NULL );
 #endif
     return (*env)->NewStringUTF( env, error );
 }
@@ -2214,7 +2216,7 @@ jint JNI_OnLoad( JavaVM *vm, void *reserved )
 #ifdef __APPLE__
 static void *apple_wine_thread( void *arg )
 {
-    start_main_thread();
+    start_main_thread( 0, NULL );
     return NULL;
 }
 
@@ -2420,5 +2422,5 @@ void __wine_main( int argc, char *argv[], char *envp[] )
 #ifdef __APPLE__
     apple_main_thread();
 #endif
-    start_main_thread();
+    start_main_thread( argc, argv );
 }
