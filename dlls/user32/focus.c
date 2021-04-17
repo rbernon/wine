ef39354bcc01dfe92c42561a6dd9adb79e74b7b3
Rémi Bernon <rbernon@codeweavers.com>
2021-04-17 19:57:30 +0200

    server: Add a timestamp to set_foreground_window requests.
    We call SetForegroundWindow in event handlers and sometimes overwrite
    the foreground window with an older event. Having a timestamp will help
    discarding old events.

--- dlls/user32/focus.c
+++ dlls/user32/focus.c
@@ -185,7 +185,7 @@ done:
 /*******************************************************************
  *		set_foreground_window
  */
-static BOOL set_foreground_window( HWND hwnd, BOOL mouse )
+static BOOL set_foreground_window( HWND hwnd, BOOL mouse, DWORD time )
 {
     BOOL ret, send_msg_old = FALSE, send_msg_new = FALSE;
     HWND previous = 0;
@@ -193,6 +193,7 @@ static BOOL set_foreground_window( HWND hwnd, BOOL mouse )
     SERVER_START_REQ( set_foreground_window )
     {
         req->handle = wine_server_user_handle( hwnd );
+        req->time = time;
         if ((ret = !wine_server_call_err( req )))
         {
             previous = wine_server_ptr_handle( reply->previous );
@@ -229,7 +230,7 @@ static BOOL set_foreground_window( HWND hwnd, BOOL mouse )
  */
 BOOL FOCUS_MouseActivate( HWND hwnd )
 {
-    return set_foreground_window( hwnd, TRUE );
+    return set_foreground_window( hwnd, TRUE, GetTickCount() );
 }
 
 
@@ -331,7 +332,7 @@ BOOL WINAPI SetForegroundWindow( HWND hwnd )
     TRACE( "%p\n", hwnd );
 
     hwnd = WIN_GetFullHandle( hwnd );
-    return set_foreground_window( hwnd, FALSE );
+    return set_foreground_window( hwnd, FALSE, GetTickCount() );
 }
 
 
