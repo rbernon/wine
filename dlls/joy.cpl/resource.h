7262008f280a1324aa808443f7b35fc11d94e60e
Rémi Bernon <rbernon@codeweavers.com>
2022-11-27 18:13:09 +0100

    joy.cpl: Add a new winmm joystick test tab.

--- dlls/joy.cpl/resource.h
+++ dlls/joy.cpl/resource.h
@@ -38,6 +38,7 @@
 #define IDD_TEST_DI         1001
 #define IDD_TEST_XI         1002
 #define IDD_TEST_WGI        1003
+#define IDD_TEST_WMM        1004
 
 #define IDC_DI_ENABLED_LIST    2000
 #define IDC_XI_ENABLED_LIST    2001
@@ -75,6 +76,9 @@
 #define IDC_WGI_DISPLAY     2302
 #define IDC_WGI_RUMBLE      2303
 #define IDC_WGI_EFFECTS     2304
+
+#define IDC_WMM_SELECT      2400
+#define IDC_WMM_DISPLAY     2401
 
 #define ICO_MAIN            100
 
