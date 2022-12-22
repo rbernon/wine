28d08cd1b8692fa71c73e26904476eeda1cc1073
Rémi Bernon <rbernon@codeweavers.com>
2022-12-22 01:57:57 +0100

    dinput: Stop checking for messages when we shouldn't.

--- dlls/dinput/device.c
+++ dlls/dinput/device.c
@@ -1456,8 +1456,6 @@ static HRESULT WINAPI dinput_device_GetDeviceState( IDirectInputDevice8W *iface,
 
     if (!data) return DIERR_INVALIDPARAM;
 
-    IDirectInputDevice2_Poll( iface );
-
     EnterCriticalSection( &impl->crit );
     if (impl->status == STATUS_UNPLUGGED)
         hr = DIERR_INPUTLOST;
@@ -1521,7 +1519,6 @@ static HRESULT WINAPI dinput_device_GetDeviceData( IDirectInputDevice8W *iface, 
         return DI_OK;
     if (size < sizeof(DIDEVICEOBJECTDATA_DX3)) return DIERR_INVALIDPARAM;
 
-    IDirectInputDevice2_Poll(iface);
     EnterCriticalSection(&This->crit);
 
     len = This->queue_head - This->queue_tail;
@@ -1769,9 +1766,7 @@ static HRESULT WINAPI dinput_device_Poll( IDirectInputDevice8W *iface )
     if (impl->status == STATUS_UNPLUGGED) hr = DIERR_INPUTLOST;
     else if (impl->status != STATUS_ACQUIRED) hr = DIERR_NOTACQUIRED;
     LeaveCriticalSection( &impl->crit );
-    if (FAILED(hr)) return hr;
 
-    if (impl->vtbl->poll) return impl->vtbl->poll( iface );
     return hr;
 }
 
