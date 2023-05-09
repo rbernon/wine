04f6bc401a8129c67dab8bc68c3149754159f05f
Rémi Bernon <rbernon@codeweavers.com>
2023-05-09 21:38:25 +0200

    introuce setcaretpos

--- dlls/win32u/input.c
+++ dlls/win32u/input.c
@@ -2436,6 +2436,8 @@ BOOL set_caret_pos( int x, int y )
         set_ime_composition_rect( hwnd, r );
         NtUserSetSystemTimer( hwnd, SYSTEM_TIMER_CARET, caret.timeout );
     }
+
+    user_driver->pSetCaretPos( &r );
     return ret;
 }
 
--- include/wine/gdi_driver.h
+++ include/wine/gdi_driver.h
@@ -341,6 +341,7 @@ struct user_driver_funcs
     UINT    (*pImeProcessKey)(HIMC,UINT,UINT,const BYTE*);
     void    (*pNotifyIMEStatus)(HWND,UINT);
     BOOL    (*pSetIMECompositionRect)(HWND,RECT);
+    void    (*pSetCaretPos)(const RECT*);
     /* cursor/icon functions */
     void    (*pDestroyCursorIcon)(HCURSOR);
     void    (*pSetCursor)(HWND,HCURSOR);
