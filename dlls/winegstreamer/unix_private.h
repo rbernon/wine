4c02d9908c342abce67271b6e2cda6253f4a4aa8
Rémi Bernon <rbernon@codeweavers.com>
2023-05-12 13:34:23 +0200

    winegstreamer: Introduce and use a new wg_task_pool helper.
    To better track GStreamer threads, avoiding potential task leaks in the
    default pool which keeps some thread alive.
    
    CW-Bug-Id: #22045

--- dlls/winegstreamer/unix_private.h
+++ dlls/winegstreamer/unix_private.h
@@ -111,6 +111,10 @@ static inline BYTE *wg_sample_data(struct wg_sample *sample)
     return (BYTE *)(UINT_PTR)sample->data;
 }
 
+/* wg_task_pool.c */
+
+extern GstTaskPool *wg_task_pool_new(void) DECLSPEC_HIDDEN;
+
 /* wg_allocator_release_sample can be used to release any sample that was requested. */
 typedef struct wg_sample *(*wg_allocator_request_sample_cb)(gsize size, void *context);
 extern GstAllocator *wg_allocator_create(void);
