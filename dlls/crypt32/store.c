f29edebdf047cf4cc883b9526ffa98e03cf0714d
Rémi Bernon <rbernon@codeweavers.com>
2023-05-29 22:08:48 +0200

    crypt32: Add a separate context method to create links.

--- dlls/crypt32/store.c
+++ dlls/crypt32/store.c
@@ -144,7 +144,8 @@ static BOOL MemStore_addContext(WINE_MEMSTORE *store, struct list *list, context
 {
     context_t *context;
 
-    context = orig_context->vtbl->clone(orig_context, &store->hdr, use_link);
+    if (use_link) context = orig_context->vtbl->link( orig_context, &store->hdr );
+    else context = orig_context->vtbl->clone( orig_context, &store->hdr );
     if (!context)
         return FALSE;
 
