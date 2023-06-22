4a1f0e3c8f5432cc6b1c4ff151bafb478d4ecfde
Rémi Bernon <rbernon@codeweavers.com>
2023-06-22 13:31:53 +0200

    shell tests

--- dlls/shell32/tests/systray.c
+++ dlls/shell32/tests/systray.c
@@ -88,6 +88,19 @@ static void test_cbsize(void)
     ok(!Shell_NotifyIconA(NIM_DELETE, &nidA) != !ret, "The icon was not deleted\n");
     ok(GetLastError() == E_FAIL || GetLastError() == ERROR_TIMEOUT,
        "GetLastError() = %lu\n", GetLastError());
+
+    /* same for Shell_NotifyIconA */
+    ZeroMemory(&nidA, sizeof(nidA));
+    nidA.cbSize = NOTIFYICONDATAA_V1_SIZE;
+    nidA.hWnd = hMainWnd;
+    nidA.uID = 1;
+    nidA.uFlags = NIF_ICON|NIF_MESSAGE;
+    nidA.hIcon = LoadIconA(NULL, (LPSTR)IDI_APPLICATION);
+    nidA.uCallbackMessage = WM_USER+17;
+    SetLastError(0xdeadbeef);
+    ok(Shell_NotifyIconA(NIM_ADD, &nidA), "NIM_ADD failed!\n");
+    ok(GetLastError() == ERROR_SUCCESS || GetLastError() == ERROR_NO_TOKEN,
+       "GetLastError() = %lu\n", GetLastError());
 }
 
 START_TEST(systray)
