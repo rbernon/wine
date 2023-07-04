155b77fcd3211b794498f57223f0acb702935595
Rémi Bernon <rbernon@codeweavers.com>
2023-07-04 12:10:26 +0200

    test bloodhunt

--- dlls/mf/tests/mf_test.h
+++ dlls/mf/tests/mf_test.h
@@ -73,6 +73,13 @@ typedef struct attribute_desc media_type_desc[32];
 #define ATTR_UINT64(k, v, ...)    {.key = &k, .name = #k, {.vt = VT_UI8, .uhVal = {.QuadPart = v}}, __VA_ARGS__ }
 #define ATTR_WSTR(k, v, ...)      {.key = &k, .name = #k, {.vt = VT_LPWSTR, .pwszVal = (WCHAR *)v}, __VA_ARGS__ }
 
+extern const char *debugstr_mf_guid(const GUID *guid);
+
+#define dump_media_type(a) dump_attributes_(__LINE__, (IMFAttributes *)a)
+#define dump_attributes(a) dump_attributes_(__LINE__, a)
+extern void dump_attributes_(int line, IMFAttributes *attributes);
+extern void dump_properties(IPropertyStore *store);
+
 #define check_media_type(a, b, c) check_attributes_(__FILE__, __LINE__, (IMFAttributes *)a, b, c)
 #define check_attributes(a, b, c) check_attributes_(__FILE__, __LINE__, a, b, c)
 extern void check_attributes_(const char *file, int line, IMFAttributes *attributes,
@@ -121,6 +128,10 @@ struct sample_desc
 #define check_mf_sample_collection(a, b, c) check_mf_sample_collection_(__FILE__, __LINE__, a, b, c, FALSE)
 extern DWORD check_mf_sample_collection_(const char *file, int line, IMFCollection *samples,
         const struct sample_desc *expect_samples, const WCHAR *expect_data_filename, BOOL use_2d_buffer);
+
+extern IMFSampleGrabberSinkCallback *create_test_grabber_callback(void);
+extern IMFAsyncCallback *create_test_callback(BOOL check_media_event);
+extern IMFPresentationClock *presentation_clock;
 
 extern IMFMediaSource *create_media_source(const WCHAR *name, const WCHAR *mime);
 extern IMFSampleGrabberSinkCallback *create_test_grabber_callback(void);
