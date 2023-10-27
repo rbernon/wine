183215330c2e1c3dd3ad869077b7eb777fe5a665
Yuxuan Shui <yshui@codeweavers.com>
2023-10-27 19:35:17 +0100

    mfmediaengine: Be a bit more conservative with locks in engine Shutdown.
    During engine shutdown we acquire engine lock first, then locks of its constituents (e.g. sample
    grabbers); whereas normally the order is the other way around (e.g. timer callback -> acquire sample
    grabber lock -> OnProcessSample callback -> engine lock). This is deadlock prone.
    
    With this commit, engine lock is released before we shutdown the inner media session.
    
    CW-Bug-Id: #22798

--- dlls/mfmediaengine/main.c
+++ dlls/mfmediaengine/main.c
@@ -2353,6 +2353,7 @@ static HRESULT WINAPI media_engine_GetVideoAspectRatio(IMFMediaEngineEx *iface, 
 static HRESULT WINAPI media_engine_Shutdown(IMFMediaEngineEx *iface)
 {
     struct media_engine *engine = impl_from_IMFMediaEngineEx(iface);
+    IMFMediaSession *session = NULL;
     HRESULT hr = S_OK;
 
     TRACE("%p.\n", iface);
@@ -2367,9 +2368,15 @@ static HRESULT WINAPI media_engine_Shutdown(IMFMediaEngineEx *iface)
     {
         media_engine_set_flag(engine, FLAGS_ENGINE_SHUT_DOWN, TRUE);
         media_engine_clear_presentation(engine);
-        IMFMediaSession_Shutdown(engine->session);
+        IMFMediaSession_AddRef(engine->session);
+        session = engine->session;
     }
 
+    if (SUCCEEDED(hr))
+    {
+        IMFMediaSession_Shutdown(session);
+        IMFMediaSession_Release(session);
+    }
     return hr;
 }
 
