67727f567ecbd80f3cbfa743fdda56dbc3ad8d5c
Rémi Bernon <rbernon@codeweavers.com>
2023-11-15 18:47:17 +0100

    winegstreamer: Call gst_audio_info_from_caps for all audio formats.

--- dlls/winegstreamer/wg_format.c
+++ dlls/winegstreamer/wg_format.c
@@ -396,16 +396,19 @@ void wg_format_from_caps(struct wg_format *format, const GstCaps *caps)
 {
     const GstStructure *structure = gst_caps_get_structure(caps, 0);
     const char *name = gst_structure_get_name(structure);
+    GstAudioInfo audio_info;
     gboolean parsed;
 
     memset(format, 0, sizeof(*format));
 
-    if (!strcmp(name, "audio/x-raw"))
+    if (g_str_has_prefix(name, "audio/") && gst_audio_info_from_caps(&audio_info, caps))
     {
-        GstAudioInfo info;
-
-        if (gst_audio_info_from_caps(&info, caps))
-            wg_format_from_audio_info(format, &info);
+        if (GST_AUDIO_INFO_FORMAT(&audio_info) != GST_AUDIO_FORMAT_ENCODED)
+            wg_format_from_audio_info(format, &audio_info);
+        else if (!strcmp(name, "audio/mpeg") && gst_structure_get_boolean(structure, "parsed", &parsed) && parsed)
+            wg_format_from_caps_audio_mpeg1(format, caps);
+        else if (!strcmp(name, "audio/x-wma"))
+            wg_format_from_caps_audio_wma(format, caps);
     }
     else if (!strcmp(name, "video/x-raw"))
     {
@@ -413,14 +416,6 @@ void wg_format_from_caps(struct wg_format *format, const GstCaps *caps)
 
         if (gst_video_info_from_caps(&info, caps))
             wg_format_from_video_info(format, &info);
-    }
-    else if (!strcmp(name, "audio/mpeg") && gst_structure_get_boolean(structure, "parsed", &parsed) && parsed)
-    {
-        wg_format_from_caps_audio_mpeg1(format, caps);
-    }
-    else if (!strcmp(name, "audio/x-wma"))
-    {
-        wg_format_from_caps_audio_wma(format, caps);
     }
     else if (!strcmp(name, "video/x-cinepak"))
     {
