b756f1bd73970252fe0553f00e956b269bf4548c
Rémi Bernon <rbernon@codeweavers.com>
2023-11-22 23:11:45 +0100

    winegstreamer/new_media_source: Push a stream and segment event to the wg_source.

--- dlls/winegstreamer/unix_private.h
+++ dlls/winegstreamer/unix_private.h
@@ -56,6 +56,7 @@ extern GstCaps *wg_format_to_caps(const struct wg_format *format);
 
 extern NTSTATUS wg_source_create(void *args);
 extern NTSTATUS wg_source_destroy(void *args);
+extern NTSTATUS wg_source_push_data(void *args);
 
 /* wg_transform.c */
 
