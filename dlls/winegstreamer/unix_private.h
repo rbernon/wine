5739985c058e5691662564cbba912a37ca5e42d7
Rémi Bernon <rbernon@codeweavers.com>
2023-11-29 18:02:55 +0100

    winegstreamer/new_media_source: Query the stream max duration from the wg_source.

--- dlls/winegstreamer/unix_private.h
+++ dlls/winegstreamer/unix_private.h
@@ -60,6 +60,7 @@ extern uint32_t wg_channel_mask_from_gst(const GstAudioInfo *info);
 extern NTSTATUS wg_source_create(void *args);
 extern NTSTATUS wg_source_destroy(void *args);
 extern NTSTATUS wg_source_get_stream_count(void *args);
+extern NTSTATUS wg_source_get_duration(void *args);
 extern NTSTATUS wg_source_get_position(void *args);
 extern NTSTATUS wg_source_push_data(void *args);
 
