a5d934036c9408a2d4d3ad9163eb62339a5eae59
Rémi Bernon <rbernon@codeweavers.com>
2024-01-27 23:27:25 +0100

    server: Register for X11 raw mouse motion events.

--- server/process.h
+++ server/process.h
@@ -116,6 +116,7 @@ extern void kill_process( struct process *process, int violent_death );
 extern void kill_console_processes( struct thread *renderer, int exit_code );
 extern void detach_debugged_processes( struct debug_obj *debug_obj, int exit_code );
 extern void enum_processes( int (*cb)(struct process*, void*), void *user);
+extern void enum_processes_for_host( struct object *host, int (*callback)( struct process *, void * ), void *user );
 
 /* console functions */
 extern struct thread *console_get_renderer( struct console *console );
