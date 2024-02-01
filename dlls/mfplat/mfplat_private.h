d6b84e9db4cedca8e5af803bd595d113fe687583
Rémi Bernon <rbernon@codeweavers.com>
2024-02-01 19:57:54 +0100

    winecrt0: Move debugstr_mf_attr to winecrtd/dbgstr_mf.c.

--- dlls/mfplat/mfplat_private.h
+++ dlls/mfplat/mfplat_private.h
@@ -27,6 +27,7 @@
 
 /* winecrtd/dbgstr_mf.c */
 extern const char *debugstr_mf_guid(const GUID *guid);
+extern const char *debugstr_mf_attr(const GUID *guid);
 
 struct attribute
 {
@@ -46,7 +47,6 @@ struct attributes
 
 extern HRESULT init_attributes_object(struct attributes *object, UINT32 size);
 extern void clear_attributes_object(struct attributes *object);
-extern const char *debugstr_attr(const GUID *guid);
 
 extern HRESULT attributes_GetItem(struct attributes *object, REFGUID key, PROPVARIANT *value);
 extern HRESULT attributes_GetItemType(struct attributes *object, REFGUID key, MF_ATTRIBUTE_TYPE *type);
