b4a35b02fa8b89dd6e0c85ed4ef200204d03ed1e
Rémi Bernon <rbernon@codeweavers.com>
2024-02-01 16:37:37 +0100

    winecrtd: Move __wine_dbg_parse_options to winecrtd/options.c.

--- dlls/winecrtd/options.c
+++ dlls/winecrtd/options.c
@@ -25,7 +25,7 @@
 #include "wine/debug.h"
 
 /* add a new debug option at the end of the option list */
-int __wine_dbg_add_option( struct __wine_debug_channel *options, int option_count, unsigned char default_flags,
+static int dbg_add_option( struct __wine_debug_channel *options, int option_count, unsigned char default_flags,
                            const char *name, unsigned char set, unsigned char clear )
 {
     struct __wine_debug_channel *tmp, *opt = options, *end = opt + option_count;
@@ -48,4 +48,69 @@ int __wine_dbg_add_option( struct __wine_debug_channel *options, int option_coun
     strcpy( opt->name, name );
     opt->flags = (default_flags & ~clear) | set;
     return option_count + 1;
+}
+
+/* parse a set of debugging option specifications and add them to the option list */
+int __wine_dbg_parse_options( struct __wine_debug_channel *options, int max_options,
+                              const char *wine_debug, const char *app_name )
+{
+    static const char *const debug_classes[] = {"fixme", "err", "warn", "trace"};
+    unsigned char default_flags = (1 << __WINE_DBCL_ERR) | (1 << __WINE_DBCL_FIXME);
+    const char *opt, *next;
+    unsigned int i, count = 0;
+
+    for (opt = wine_debug; opt; opt = next)
+    {
+        struct __wine_debug_channel option = {0};
+        const char *p, *end;
+        unsigned char set = 0, clear = 0;
+
+        if ((next = strchr( opt, ',' ))) end = next++;
+        else end = opt + strlen( opt );
+
+        if ((p = strchr( opt, ':' )))
+        {
+            if (strncasecmp( opt, app_name, p - opt )) continue;
+            opt = p + 1;
+        }
+
+        p = opt + strcspn( opt, "+-" );
+        if (p == end) p = opt;  /* assume it's a debug channel name */
+
+        if (p > opt)
+        {
+            for (i = 0; i < ARRAY_SIZE( debug_classes ); i++)
+            {
+                int len = strlen( debug_classes[i] );
+                if (len != (p - opt)) continue;
+                if (!memcmp( opt, debug_classes[i], len ))  /* found it */
+                {
+                    if (*p == '+') set |= 1 << i;
+                    else clear |= 1 << i;
+                    break;
+                }
+            }
+            if (i == ARRAY_SIZE( debug_classes )) /* bad class name, skip it */
+                continue;
+        }
+        else
+        {
+            if (*p == '-') clear = ~0;
+            else set = ~0;
+        }
+        if (*p == '+' || *p == '-') p++;
+        if (end - p >= sizeof(option.name)) continue; /* name too long */
+        memcpy( option.name, p, end - p );
+
+        if (!strcmp( option.name, "all" ) || !option.name[0])
+            default_flags = (default_flags & ~clear) | set;
+        else
+        {
+            count = dbg_add_option( options, count, default_flags, option.name, set, clear );
+            if (count >= max_options - 1) break; /* too many options */
+        }
+    }
+
+    options[count++].flags = default_flags;
+    return count;
 }
