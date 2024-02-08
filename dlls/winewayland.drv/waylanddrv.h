0413b2a51eb4bd6590672dc1720bc8cc264f6dfe
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2024-02-08 12:46:12 +0200

    winewayland.drv: Ignore non-current wl_output modes.
    The reporting of non-current wl_output modes is deprecated, and most
    compositors now report only the current display mode.

--- dlls/winewayland.drv/waylanddrv.h
+++ dlls/winewayland.drv/waylanddrv.h
@@ -155,7 +155,6 @@ struct wayland
 
 struct wayland_output_mode
 {
-    struct rb_entry entry;
     int32_t width;
     int32_t height;
     int32_t refresh;
@@ -163,9 +162,7 @@ struct wayland_output_mode
 
 struct wayland_output_state
 {
-    int modes_count;
-    struct rb_tree modes;
-    struct wayland_output_mode *current_mode;
+    struct wayland_output_mode mode;
     char *name;
     int logical_x, logical_y;
     int logical_w, logical_h;
