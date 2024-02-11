e1dd28bc2c80da53b72eadfca00c829f7b8b6689
Rémi Bernon <rbernon@codeweavers.com>
2024-02-11 10:04:14 +0100

    get_prefix_dir

--- sommelier/src/main.rs
+++ sommelier/src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+use std::path;
+
+fn get_prefix_dir() -> path::PathBuf {
+    match env::var("WINEPREFIX") {
+        Ok(val) => path::PathBuf::from(&val),
+        _ => match env::var("HOME") {
+            Ok(val) => path::Path::new(&val).join(".wine"),
+            _ => panic!("Missing WINEPREFIX or HOME environment variables"),
+        },
+    }
+}
+
+fn create_server_dir() {}
+
 fn main() {
     println!("Hello, world!");
+    println!("{}", get_prefix_dir().display());
 }
