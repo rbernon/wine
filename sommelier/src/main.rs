def6a9afa2bd4f6a4774c4fa6efd1cfece091b8d
Rémi Bernon <rbernon@codeweavers.com>
2024-02-11 14:44:02 +0100

    sendmsg

--- sommelier/src/main.rs
+++ sommelier/src/main.rs
@@ -1,20 +1,99 @@
-#![feature(unix_socket_ancillary_data)]
-
 use std::env;
-use std::ffi;
 use std::fs;
-use std::io;
+use std::mem;
 use std::path;
+use std::ptr;
 use std::rc;
 use std::thread;
 
+use std::os::fd::AsRawFd;
 use std::os::fd::FromRawFd;
 use std::os::linux::fs::MetadataExt;
 use std::os::unix::fs::PermissionsExt;
-use std::os::unix::net::{SocketAncillary, UnixListener, UnixStream};
+use std::os::unix::net::{UnixListener, UnixStream};
 
 const SOCKET_NAME: &str = "socket";
 const SERVER_PROTOCOL_VERSION: u32 = 1;
+
+#[repr(C)]
+struct iovec {
+    iov_base: *mut u8,
+    iov_len: usize,
+}
+
+#[repr(C)]
+struct cmsghdr {
+    cmsg_len: usize,
+    cmsg_level: i32,
+    cmsg_type: i32,
+}
+
+#[repr(C)]
+struct msghdr {
+    msg_name: *mut u8,
+    msg_namelen: u32,
+    msg_iov: *mut iovec,
+    msg_iovlen: usize,
+    msg_control: *mut cmsghdr,
+    msg_controllen: usize,
+    msg_flags: i32,
+}
+
+extern "C" {
+    fn pipe(fds: *mut i32) -> i32;
+    fn close(fd: i32) -> i32;
+    fn sendmsg(fd: i32, msg: *const msghdr, flags: i32) -> isize;
+}
+
+fn send_client_fd(stream: UnixStream, fd: i32, handle: u32) {
+    #[cfg(target_pointer_width = "32")]
+    #[repr(C, align(4))]
+    struct cmsg {
+        hdr: cmsghdr,
+        fd: i32,
+    }
+
+    #[cfg(target_pointer_width = "64")]
+    #[repr(C, align(8))]
+    struct cmsg {
+        hdr: cmsghdr,
+        fd: i32,
+    }
+
+    let mut ctrl = cmsg {
+        hdr: cmsghdr {
+            cmsg_len: mem::size_of::<cmsg>(),
+            cmsg_level: 1, /*SOL_SOCKET*/
+            cmsg_type: 1,  /*SCM_RIGHTS*/
+        },
+        fd: fd,
+    };
+
+    let msg = msghdr {
+        msg_name: ptr::null_mut(),
+        msg_namelen: 0,
+        msg_iov: &mut iovec {
+            iov_base: handle.to_le_bytes().as_mut_ptr(),
+            iov_len: mem::size_of_val(&handle),
+        },
+        msg_iovlen: 1,
+        msg_control: &mut ctrl.hdr,
+        msg_controllen: ctrl.hdr.cmsg_len,
+        msg_flags: 0,
+    };
+
+    assert!(unsafe { sendmsg(stream.as_raw_fd(), &msg, 0) } != -1);
+}
+
+fn send_process_pipe(stream: UnixStream) -> UnixStream {
+    let mut fds = [-1, -1];
+
+    assert!(unsafe { pipe(fds.as_mut_ptr()) } != -1);
+    send_client_fd(stream, fds[1], SERVER_PROTOCOL_VERSION);
+    assert!(unsafe { close(fds[1]) } != -1);
+
+    return unsafe { UnixStream::from_raw_fd(fds[0]) };
+}
 
 fn get_prefix_dir() -> path::PathBuf {
     match env::var("WINEPREFIX") {
@@ -66,32 +145,6 @@ struct Thread {
 struct Process {
     stream: UnixStream,
     threads: Vec<Thread>,
-}
-
-extern "C" {
-    fn pipe(fds: *mut ffi::c_int) -> ffi::c_int;
-    fn close(fd: ffi::c_int) -> ffi::c_int;
-}
-
-fn send_client_fd(stream: UnixStream, fd: ffi::c_int, handle: u32) {
-    let data: [u8; 4] = handle.to_le_bytes();
-    let data_buf = &[io::IoSlice::new(&data)];
-
-    let mut ancillary_buf = [0; 128];
-    let mut ancillary = SocketAncillary::new(&mut ancillary_buf);
-    ancillary.add_fds(&[fd]);
-
-    stream.send_vectored_with_ancillary(data_buf, &mut ancillary).unwrap();
-}
-
-fn send_process_pipe(stream: UnixStream) -> UnixStream {
-    let mut fds: [ffi::c_int; 2] = [-1, -1];
-
-    assert!(unsafe { pipe(&mut fds as *mut ffi::c_int) } != -1);
-    send_client_fd(stream, fds[1], SERVER_PROTOCOL_VERSION);
-    assert!(unsafe { close(fds[1]) } != -1);
-
-    return unsafe { UnixStream::from_raw_fd(fds[0]) };
 }
 
 fn handle_client(stream: UnixStream) {
