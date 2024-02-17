9fa62a668eb40edc67e123d5dfe12313249a5eb2
Rémi Bernon <rbernon@codeweavers.com>
2024-02-18 00:34:01 +0100

    root dir

--- sommelier/src/mapping.rs
+++ sommelier/src/mapping.rs
@@ -12,10 +12,6 @@ impl Mapping {
     pub fn new() -> Arc<Mapping> {
         Arc::new_cyclic(|weak| Mapping { weak: weak.clone() })
     }
-
-    pub fn open(&self, process: &mut Process) -> Handle {
-        process.handles.alloc(&self.grab())
-    }
 }
 
 impl KernelObject for Mapping {
