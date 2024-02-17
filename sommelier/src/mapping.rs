d043057422e6ce581d60eb7aef859fb244f972f5
Rémi Bernon <rbernon@codeweavers.com>
2024-02-17 23:49:45 +0100

    make_permanent / temporary

--- sommelier/src/mapping.rs
+++ sommelier/src/mapping.rs
@@ -10,9 +10,7 @@ pub struct Mapping {
 
 impl Mapping {
     pub fn new() -> Arc<Mapping> {
-        Arc::new_cyclic(|weak| {
-            Mapping { weak: weak.clone() }
-        })
+        Arc::new_cyclic(|weak| Mapping { weak: weak.clone() })
     }
 
     pub fn open(&self, process: &mut Process) -> Handle {
