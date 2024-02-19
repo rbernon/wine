5f10d3025a64eb63baccdb5c3ea2fbbd6fb4d00d
Rémi Bernon <rbernon@codeweavers.com>
2024-02-19 22:27:55 +0100

    server: Get rid of the global cursor structure.
    Based on a patch by Huw Davies <huw@codeweavers.com>.

--- server/user.h
+++ server/user.h
@@ -54,12 +54,6 @@ struct winstation
     struct namespace  *desktop_names;      /* namespace for desktops of this winstation */
 };
 
-struct global_cursor
-{
-    unsigned int         clip_flags;       /* last cursor clip flags */
-    user_handle_t        win;              /* window that contains the cursor */
-};
-
 struct desktop
 {
     struct object        obj;              /* object header */
@@ -76,8 +70,9 @@ struct desktop
     struct timeout_user *close_timeout;    /* timeout before closing the desktop */
     struct thread_input *foreground_input; /* thread input of foreground thread */
     unsigned int         users;            /* processes and threads using this desktop */
-    struct global_cursor cursor;           /* global cursor information */
     unsigned char        keystate[256];    /* asynchronous key state */
+    user_handle_t        cursor_win;       /* window that contains the cursor */
+    unsigned int         clip_flags;       /* last cursor clip flags */
     int                  session_index;    /* desktop index in session shared memory */
 };
 
