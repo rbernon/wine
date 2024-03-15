45fb5df2d5a64f0afdfb45228837dd6711fe459a
Rémi Bernon <rbernon@codeweavers.com>
2024-03-15 14:21:21 +0100

    server: Clear queue QS_RAWINPUT if get_rawinput_buffer got everything.

--- server/queue.c
+++ server/queue.c
@@ -3868,6 +3868,7 @@ DECL_HANDLER(get_rawinput_buffer)
     else if ((buffer = mem_alloc( buffer_size )))
     {
         size_t total_size = 0, next_size = 0;
+        int clear = 1;
 
         reply->next_size = get_reply_max_size();
 
@@ -3882,6 +3883,7 @@ DECL_HANDLER(get_rawinput_buffer)
                 if (total_size + sizeof(RAWINPUTHEADER) + data_size > buffer_size)
                 {
                     next_size = sizeof(RAWINPUTHEADER) + data_size;
+                    clear = 0;
                     break;
                 }
 
@@ -3896,6 +3898,10 @@ DECL_HANDLER(get_rawinput_buffer)
                 list_remove( &msg->entry );
                 free_message( msg );
                 count++;
+            }
+            else if (get_hardware_msg_bit( msg->msg ) == QS_RAWINPUT)
+            {
+                clear = 0;
             }
         }
 
@@ -3913,6 +3919,7 @@ DECL_HANDLER(get_rawinput_buffer)
 
         reply->count = count;
         set_reply_data_ptr( buffer, total_size );
+        if (clear) clear_queue_bits( current->queue, QS_RAWINPUT );
     }
 }
 
