225dd55cc630c037789e504ff9b6160eb124dbc9
Rémi Bernon <rbernon@codeweavers.com>
2024-05-28 14:40:18 +0200

    winex11: FIXME DPI

--- dlls/winex11.drv/x11drv.h
+++ dlls/winex11.drv/x11drv.h
@@ -826,7 +826,7 @@ extern void xim_set_focus( HWND hwnd, BOOL focus );
 
 static inline BOOL is_window_rect_mapped( const RECT *rect )
 {
-    RECT virtual_rect = NtUserGetVirtualScreenRect();
+    RECT virtual_rect = NtUserGetVirtualScreenRect(); /* FIXME DPI */
     return (rect->left < virtual_rect.right &&
             rect->top < virtual_rect.bottom &&
             max( rect->right, rect->left + 1 ) > virtual_rect.left &&
