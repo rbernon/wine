32fa76a79133ead23ea88dd0e7e625ba23946942
Rémi Bernon <rbernon@codeweavers.com>
2024-06-01 22:06:41 +0200

    win32u: Set per monitor aware DPI while processing driver messages.

--- dlls/win32u/message.c
+++ dlls/win32u/message.c
@@ -3041,9 +3041,11 @@ static inline void check_for_driver_events(void)
 {
     if (get_user_thread_info()->last_driver_time != NtGetTickCount())
     {
+        UINT context = set_thread_dpi_awareness_context( NTUSER_DPI_PER_MONITOR_AWARE_V2 );
         flush_window_surfaces( FALSE );
         user_driver->pProcessEvents( QS_ALLINPUT );
         get_user_thread_info()->last_driver_time = NtGetTickCount();
+        set_thread_dpi_awareness_context( context );
     }
 }
 
@@ -3063,6 +3065,7 @@ static DWORD wait_message( DWORD count, const HANDLE *handles, DWORD timeout, DW
     DWORD ret;
     void *ret_ptr;
     ULONG ret_len;
+    UINT context;
 
     if (!KeUserDispatchCallback( &params.dispatch, sizeof(params), &ret_ptr, &ret_len ) &&
         ret_len == sizeof(params.locks))
@@ -3071,6 +3074,7 @@ static DWORD wait_message( DWORD count, const HANDLE *handles, DWORD timeout, DW
         params.restore = TRUE;
     }
 
+    context = set_thread_dpi_awareness_context( NTUSER_DPI_PER_MONITOR_AWARE_V2 );
     if (user_driver->pProcessEvents( mask )) ret = count - 1;
     else
     {
@@ -3083,6 +3087,7 @@ static DWORD wait_message( DWORD count, const HANDLE *handles, DWORD timeout, DW
             ret = WAIT_FAILED;
         }
     }
+    set_thread_dpi_awareness_context( context );
 
     if (ret == WAIT_TIMEOUT && !count && !timeout) NtYieldExecution();
     if (ret == count - 1) get_user_thread_info()->last_driver_time = NtGetTickCount();
