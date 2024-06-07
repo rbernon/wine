f193a3a2ea0d322ae720eed4577f27bd9ffdabe8
Rémi Bernon <rbernon@codeweavers.com>
2024-06-07 13:23:01 +0200

    winecrt0: Implement static constructors support.

--- dlls/win32u/main.c
+++ dlls/win32u/main.c
@@ -2356,3 +2356,8 @@ BOOL WINAPI DllMain( HINSTANCE inst, DWORD reason, void *reserved )
     }
     return TRUE;
 }
+
+BOOL WINAPI DllMainCRTStartup( HINSTANCE inst, DWORD reason, void *reserved )
+{
+    return DllMain( inst, reason, reserved );
+}
