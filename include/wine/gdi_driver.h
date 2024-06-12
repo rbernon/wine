592bb998fc50d87dca9affa44c05fd75c055340c
Rémi Bernon <rbernon@codeweavers.com>
2024-06-12 12:18:32 +0200

    Revert "win32u: cairodrv"
    This reverts commit b9d14ffda75993b914152e62008786f174cb73e0.

--- include/wine/gdi_driver.h
+++ include/wine/gdi_driver.h
@@ -215,7 +215,6 @@ struct gdi_dc_funcs
     BOOL     (*pStrokeAndFillPath)(PHYSDEV);
     BOOL     (*pStrokePath)(PHYSDEV);
     BOOL     (*pUnrealizePalette)(HPALETTE);
-    void     (*pSetWindowRegion)(PHYSDEV,HWND,HWND,const RECT*,const RECT*);
 
     /* priority order for the driver on the stack */
     UINT       priority;
@@ -400,17 +399,5 @@ W32KAPI void __wine_set_user_driver( const struct user_driver_funcs *funcs, UINT
 
 W32KAPI BOOL win32u_set_window_pixel_format( HWND hwnd, int format, BOOL internal );
 W32KAPI int win32u_get_window_pixel_format( HWND hwnd );
-
-typedef void *cairodrv_window_t;
-
-struct cairodrv_funcs
-{
-    const struct gdi_dc_funcs *gdi_funcs;
-    void (*window_create)( HWND hwnd, void *cairo_surface );
-    void (*window_destroy)( HWND hwnd );
-    struct window_surface *(*surface_create)( HWND hwnd, const RECT *rect, COLORREF key, BOOL alpha );
-};
-
-W32KAPI const struct cairodrv_funcs *__wine_get_cairo_driver( UINT version );
 
 #endif /* __WINE_WINE_GDI_DRIVER_H */
