5cdfbc9c3ce4ace2cab9ac5335301681a523c230
Rémi Bernon <rbernon@codeweavers.com>
2024-06-12 11:48:46 +0200

    Revert "dwm: Add new service."
    This reverts commit b6a7abed4486469f6ca686b19d22b61715f86d44.

--- dlls/winex11.drv/x11drv.h
+++ dlls/winex11.drv/x11drv.h
@@ -424,7 +424,6 @@ extern BOOL keyboard_grabbed;
 extern unsigned int screen_bpp;
 extern BOOL usexrandr;
 extern BOOL usexvidmode;
-extern BOOL use_dwm;
 extern BOOL use_take_focus;
 extern BOOL use_primary_selection;
 extern BOOL use_system_cursors;
@@ -440,8 +439,6 @@ extern int xrender_error_base;
 extern char *process_name;
 extern Display *clipboard_display;
 extern WNDPROC client_foreign_window_proc;
-extern const struct dwm_funcs *dwm_funcs;
-extern dwm_display_t dwm_display;
 
 /* atoms */
 
@@ -602,7 +599,6 @@ struct x11drv_win_data
     HWND        hwnd;           /* hwnd that this private data belongs to */
     Window      whole_window;   /* X window for the complete window */
     Window      client_window;  /* X window for the client area */
-    dwm_window_t dwm_window;
     struct window_rects rects;  /* window rects in monitor DPI, relative to parent client area */
     XIC         xic;            /* X input context */
     UINT        managed : 1;    /* is window managed? */
