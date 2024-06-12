51afa6f0e9d26eec959deb56bfe93f5b5e0f19b9
Rémi Bernon <rbernon@codeweavers.com>
2024-06-12 11:49:52 +0200

    win32u: cairodrv

--- dlls/win32u/emfdrv.c
+++ dlls/win32u/emfdrv.c
@@ -519,6 +519,7 @@ static const struct gdi_dc_funcs emfdrv_driver =
     EMFDRV_StrokeAndFillPath,        /* pStrokeAndFillPath */
     EMFDRV_StrokePath,               /* pStrokePath */
     NULL,                            /* pUnrealizePalette */
+    NULL,                            /* pSetWindowRegion */
     GDI_PRIORITY_GRAPHICS_DRV,       /* priority */
     "emfdrv",
 };
