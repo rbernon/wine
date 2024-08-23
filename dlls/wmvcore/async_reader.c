901804a2623ac56680e6f20805f88fe888adba85
Rémi Bernon <rbernon@codeweavers.com>
2024-08-23 16:51:50 +0200

    wmvcore: Move IWMSyncReader functions to sync_reader.c.

--- dlls/wmvcore/async_reader.c
+++ dlls/wmvcore/async_reader.c
@@ -1999,7 +1999,7 @@ static HRESULT WINAPI async_reader_create(IWMReader **reader)
     object->IWMReaderTypeNegotiation_iface.lpVtbl = &WMReaderTypeNegotiationVtbl;
     object->refcount = 1;
 
-    if (FAILED(hr = winegstreamer_create_wm_sync_reader((IUnknown *)&object->IWMReader_iface,
+    if (FAILED(hr = sync_reader_create((IUnknown *)&object->IWMReader_iface,
             (void **)&object->reader_inner)))
         goto failed;
 
