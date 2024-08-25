bbda47b0abab4f72225df6fe791951a766e466a6
Rémi Bernon <rbernon@codeweavers.com>
2024-08-25 15:28:13 +0200

    include: Print test failures with wine_dbg_vprintf.

--- programs/winetest/main.c
+++ programs/winetest/main.c
@@ -1690,6 +1690,7 @@ int __cdecl main( int argc, char *argv[] )
             SetEnvironmentVariableA( "WINETEST_DEBUG", "1" );
             SetEnvironmentVariableA( "WINETEST_INTERACTIVE", "0" );
             SetEnvironmentVariableA( "WINETEST_REPORT_SUCCESS", "0" );
+            SetEnvironmentVariableA( "WINETEST_PRINT_STDOUT", "1" );
         }
         if (junit)
         {
