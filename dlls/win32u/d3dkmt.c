503a3fd7696ce7b0121c51e741f3e3e17c06011e
Rémi Bernon <rbernon@codeweavers.com>
2024-09-03 09:59:36 +0200

    WIP: win32u: Stub D3DKMT keyed mutex locking functions.

--- dlls/win32u/d3dkmt.c
+++ dlls/win32u/d3dkmt.c
@@ -909,6 +909,78 @@ NTSTATUS WINAPI NtGdiDdDDIOpenKeyedMutexFromNtHandle( D3DKMT_OPENKEYEDMUTEXFROMN
     return STATUS_NOT_IMPLEMENTED;
 }
 
+typedef struct _D3DKMT_ACQUIREKEYEDMUTEX
+{
+    D3DKMT_HANDLE hKeyedMutex;
+    UINT64 Key;
+    PLARGE_INTEGER pTimeout;
+    UINT64 FenceValue;
+} D3DKMT_ACQUIREKEYEDMUTEX;
+
+/******************************************************************************
+ *           NtGdiDdDDIAcquireKeyedMutex    (win32u.@)
+ */
+NTSTATUS WINAPI NtGdiDdDDIAcquireKeyedMutex( D3DKMT_ACQUIREKEYEDMUTEX *params )
+{
+    FIXME( "params %p stub!\n", params );
+    params->FenceValue = 0;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+typedef struct _D3DKMT_ACQUIREKEYEDMUTEX2
+{
+    D3DKMT_HANDLE hKeyedMutex;
+    UINT64 Key;
+    PLARGE_INTEGER pTimeout;
+    UINT64 FenceValue;
+    void *pPrivateRuntimeData;
+    UINT PrivateRuntimeDataSize;
+} D3DKMT_ACQUIREKEYEDMUTEX2;
+
+/******************************************************************************
+ *           NtGdiDdDDIAcquireKeyedMutex2    (win32u.@)
+ */
+NTSTATUS WINAPI NtGdiDdDDIAcquireKeyedMutex2( D3DKMT_ACQUIREKEYEDMUTEX2 *params )
+{
+    FIXME( "params %p stub!\n", params );
+    params->FenceValue = 0;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+typedef struct _D3DKMT_RELEASEKEYEDMUTEX
+{
+    D3DKMT_HANDLE hKeyedMutex;
+    UINT64 Key;
+    UINT64 FenceValue;
+} D3DKMT_RELEASEKEYEDMUTEX;
+
+/******************************************************************************
+ *           NtGdiDdDDIReleaseKeyedMutex    (win32u.@)
+ */
+NTSTATUS WINAPI NtGdiDdDDIReleaseKeyedMutex( D3DKMT_RELEASEKEYEDMUTEX *params )
+{
+    FIXME( "params %p stub!\n", params );
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+typedef struct _D3DKMT_RELEASEKEYEDMUTEX2
+{
+    D3DKMT_HANDLE hKeyedMutex;
+    UINT64 Key;
+    UINT64 FenceValue;
+    void *pPrivateRuntimeData;
+    UINT PrivateRuntimeDataSize;
+} D3DKMT_RELEASEKEYEDMUTEX2;
+
+/******************************************************************************
+ *           NtGdiDdDDIReleaseKeyedMutex2    (win32u.@)
+ */
+NTSTATUS WINAPI NtGdiDdDDIReleaseKeyedMutex2( D3DKMT_RELEASEKEYEDMUTEX2 *params )
+{
+    FIXME( "params %p stub!\n", params );
+    return STATUS_NOT_IMPLEMENTED;
+}
+
 
 /******************************************************************************
  *           NtGdiDdDDICreateSynchronizationObject2    (win32u.@)
