f0707a3bf63554db25ef38bb7f34b536a8ddbc79
Rémi Bernon <rbernon@codeweavers.com>
2024-09-21 16:32:50 +0200

    winevulkan: Disable the VK_INTEL_performance_query extension.

--- dlls/winevulkan/loader_thunks.h
+++ dlls/winevulkan/loader_thunks.h
@@ -25,7 +25,6 @@ enum unix_call
     unix_is_available_device_function,
     unix_vkAcquireNextImage2KHR,
     unix_vkAcquireNextImageKHR,
-    unix_vkAcquirePerformanceConfigurationINTEL,
     unix_vkAcquireProfilingLockKHR,
     unix_vkAllocateCommandBuffers,
     unix_vkAllocateDescriptorSets,
@@ -237,9 +236,6 @@ enum unix_call
     unix_vkCmdSetLogicOpEXT,
     unix_vkCmdSetLogicOpEnableEXT,
     unix_vkCmdSetPatchControlPointsEXT,
-    unix_vkCmdSetPerformanceMarkerINTEL,
-    unix_vkCmdSetPerformanceOverrideINTEL,
-    unix_vkCmdSetPerformanceStreamMarkerINTEL,
     unix_vkCmdSetPolygonModeEXT,
     unix_vkCmdSetPrimitiveRestartEnable,
     unix_vkCmdSetPrimitiveRestartEnableEXT,
@@ -491,7 +487,6 @@ enum unix_call
     unix_vkGetLatencyTimingsNV,
     unix_vkGetMemoryHostPointerPropertiesEXT,
     unix_vkGetMicromapBuildSizesEXT,
-    unix_vkGetPerformanceParameterINTEL,
     unix_vkGetPhysicalDeviceCalibrateableTimeDomainsEXT,
     unix_vkGetPhysicalDeviceCalibrateableTimeDomainsKHR,
     unix_vkGetPhysicalDeviceCooperativeMatrixPropertiesKHR,
@@ -571,7 +566,6 @@ enum unix_call
     unix_vkGetSwapchainImagesKHR,
     unix_vkGetValidationCacheDataEXT,
     unix_vkGetVideoSessionMemoryRequirementsKHR,
-    unix_vkInitializePerformanceApiINTEL,
     unix_vkInvalidateMappedMemoryRanges,
     unix_vkLatencySleepNV,
     unix_vkMapMemory,
@@ -584,13 +578,11 @@ enum unix_call
     unix_vkQueueInsertDebugUtilsLabelEXT,
     unix_vkQueueNotifyOutOfBandNV,
     unix_vkQueuePresentKHR,
-    unix_vkQueueSetPerformanceConfigurationINTEL,
     unix_vkQueueSubmit,
     unix_vkQueueSubmit2,
     unix_vkQueueSubmit2KHR,
     unix_vkQueueWaitIdle,
     unix_vkReleaseCapturedPipelineDataKHR,
-    unix_vkReleasePerformanceConfigurationINTEL,
     unix_vkReleaseProfilingLockKHR,
     unix_vkReleaseSwapchainImagesEXT,
     unix_vkResetCommandBuffer,
@@ -615,7 +607,6 @@ enum unix_call
     unix_vkTransitionImageLayoutEXT,
     unix_vkTrimCommandPool,
     unix_vkTrimCommandPoolKHR,
-    unix_vkUninitializePerformanceApiINTEL,
     unix_vkUnmapMemory,
     unix_vkUnmapMemory2KHR,
     unix_vkUpdateDescriptorSetWithTemplate,
@@ -649,14 +640,6 @@ struct vkAcquireNextImageKHR_params
     VkSemaphore DECLSPEC_ALIGN(8) semaphore;
     VkFence DECLSPEC_ALIGN(8) fence;
     uint32_t *pImageIndex;
-    VkResult result;
-};
-
-struct vkAcquirePerformanceConfigurationINTEL_params
-{
-    VkDevice device;
-    const VkPerformanceConfigurationAcquireInfoINTEL *pAcquireInfo;
-    VkPerformanceConfigurationINTEL *pConfiguration;
     VkResult result;
 };
 
@@ -2242,27 +2225,6 @@ struct vkCmdSetPatchControlPointsEXT_params
 {
     VkCommandBuffer commandBuffer;
     uint32_t patchControlPoints;
-};
-
-struct vkCmdSetPerformanceMarkerINTEL_params
-{
-    VkCommandBuffer commandBuffer;
-    const VkPerformanceMarkerInfoINTEL *pMarkerInfo;
-    VkResult result;
-};
-
-struct vkCmdSetPerformanceOverrideINTEL_params
-{
-    VkCommandBuffer commandBuffer;
-    const VkPerformanceOverrideInfoINTEL *pOverrideInfo;
-    VkResult result;
-};
-
-struct vkCmdSetPerformanceStreamMarkerINTEL_params
-{
-    VkCommandBuffer commandBuffer;
-    const VkPerformanceStreamMarkerInfoINTEL *pMarkerInfo;
-    VkResult result;
 };
 
 struct vkCmdSetPolygonModeEXT_params
@@ -4239,14 +4201,6 @@ struct vkGetMicromapBuildSizesEXT_params
     VkMicromapBuildSizesInfoEXT *pSizeInfo;
 };
 
-struct vkGetPerformanceParameterINTEL_params
-{
-    VkDevice device;
-    VkPerformanceParameterTypeINTEL parameter;
-    VkPerformanceValueINTEL *pValue;
-    VkResult result;
-};
-
 struct vkGetPhysicalDeviceCalibrateableTimeDomainsEXT_params
 {
     VkPhysicalDevice physicalDevice;
@@ -4883,13 +4837,6 @@ struct vkGetVideoSessionMemoryRequirementsKHR_params
     VkResult result;
 };
 
-struct vkInitializePerformanceApiINTEL_params
-{
-    VkDevice device;
-    const VkInitializePerformanceApiInfoINTEL *pInitializeInfo;
-    VkResult result;
-};
-
 struct vkInvalidateMappedMemoryRanges_params
 {
     VkDevice device;
@@ -4982,13 +4929,6 @@ struct vkQueuePresentKHR_params
     VkResult result;
 };
 
-struct vkQueueSetPerformanceConfigurationINTEL_params
-{
-    VkQueue queue;
-    VkPerformanceConfigurationINTEL DECLSPEC_ALIGN(8) configuration;
-    VkResult result;
-};
-
 struct vkQueueSubmit_params
 {
     VkQueue queue;
@@ -5027,13 +4967,6 @@ struct vkReleaseCapturedPipelineDataKHR_params
     VkDevice device;
     const VkReleaseCapturedPipelineDataInfoKHR *pInfo;
     const VkAllocationCallbacks *pAllocator;
-    VkResult result;
-};
-
-struct vkReleasePerformanceConfigurationINTEL_params
-{
-    VkDevice device;
-    VkPerformanceConfigurationINTEL DECLSPEC_ALIGN(8) configuration;
     VkResult result;
 };
 
@@ -5216,11 +5149,6 @@ struct vkTrimCommandPoolKHR_params
     VkDevice device;
     VkCommandPool DECLSPEC_ALIGN(8) commandPool;
     VkCommandPoolTrimFlags flags;
-};
-
-struct vkUninitializePerformanceApiINTEL_params
-{
-    VkDevice device;
 };
 
 struct vkUnmapMemory_params
--- dlls/winevulkan/vulkan_thunks.h
+++ dlls/winevulkan/vulkan_thunks.h
@@ -81,7 +81,6 @@ struct vulkan_device_funcs
 {
     VkResult (*p_vkAcquireNextImage2KHR)(VkDevice, const VkAcquireNextImageInfoKHR *, uint32_t *);
     VkResult (*p_vkAcquireNextImageKHR)(VkDevice, VkSwapchainKHR, uint64_t, VkSemaphore, VkFence, uint32_t *);
-    VkResult (*p_vkAcquirePerformanceConfigurationINTEL)(VkDevice, const VkPerformanceConfigurationAcquireInfoINTEL *, VkPerformanceConfigurationINTEL *);
     VkResult (*p_vkAcquireProfilingLockKHR)(VkDevice, const VkAcquireProfilingLockInfoKHR *);
     VkResult (*p_vkAllocateCommandBuffers)(VkDevice, const VkCommandBufferAllocateInfo *, VkCommandBuffer *);
     VkResult (*p_vkAllocateDescriptorSets)(VkDevice, const VkDescriptorSetAllocateInfo *, VkDescriptorSet *);
@@ -293,9 +292,6 @@ struct vulkan_device_funcs
     void (*p_vkCmdSetLogicOpEXT)(VkCommandBuffer, VkLogicOp);
     void (*p_vkCmdSetLogicOpEnableEXT)(VkCommandBuffer, VkBool32);
     void (*p_vkCmdSetPatchControlPointsEXT)(VkCommandBuffer, uint32_t);
-    VkResult (*p_vkCmdSetPerformanceMarkerINTEL)(VkCommandBuffer, const VkPerformanceMarkerInfoINTEL *);
-    VkResult (*p_vkCmdSetPerformanceOverrideINTEL)(VkCommandBuffer, const VkPerformanceOverrideInfoINTEL *);
-    VkResult (*p_vkCmdSetPerformanceStreamMarkerINTEL)(VkCommandBuffer, const VkPerformanceStreamMarkerInfoINTEL *);
     void (*p_vkCmdSetPolygonModeEXT)(VkCommandBuffer, VkPolygonMode);
     void (*p_vkCmdSetPrimitiveRestartEnable)(VkCommandBuffer, VkBool32);
     void (*p_vkCmdSetPrimitiveRestartEnableEXT)(VkCommandBuffer, VkBool32);
@@ -529,7 +525,6 @@ struct vulkan_device_funcs
     void (*p_vkGetLatencyTimingsNV)(VkDevice, VkSwapchainKHR, VkGetLatencyMarkerInfoNV *);
     VkResult (*p_vkGetMemoryHostPointerPropertiesEXT)(VkDevice, VkExternalMemoryHandleTypeFlagBits, const void *, VkMemoryHostPointerPropertiesEXT *);
     void (*p_vkGetMicromapBuildSizesEXT)(VkDevice, VkAccelerationStructureBuildTypeKHR, const VkMicromapBuildInfoEXT *, VkMicromapBuildSizesInfoEXT *);
-    VkResult (*p_vkGetPerformanceParameterINTEL)(VkDevice, VkPerformanceParameterTypeINTEL, VkPerformanceValueINTEL *);
     VkResult (*p_vkGetPipelineBinaryDataKHR)(VkDevice, const VkPipelineBinaryDataInfoKHR *, VkPipelineBinaryKeyKHR *, size_t *, void *);
     VkResult (*p_vkGetPipelineCacheData)(VkDevice, VkPipelineCache, size_t *, void *);
     VkResult (*p_vkGetPipelineExecutableInternalRepresentationsKHR)(VkDevice, const VkPipelineExecutableInfoKHR *, uint32_t *, VkPipelineExecutableInternalRepresentationKHR *);
@@ -560,7 +555,6 @@ struct vulkan_device_funcs
     VkResult (*p_vkGetSwapchainImagesKHR)(VkDevice, VkSwapchainKHR, uint32_t *, VkImage *);
     VkResult (*p_vkGetValidationCacheDataEXT)(VkDevice, VkValidationCacheEXT, size_t *, void *);
     VkResult (*p_vkGetVideoSessionMemoryRequirementsKHR)(VkDevice, VkVideoSessionKHR, uint32_t *, VkVideoSessionMemoryRequirementsKHR *);
-    VkResult (*p_vkInitializePerformanceApiINTEL)(VkDevice, const VkInitializePerformanceApiInfoINTEL *);
     VkResult (*p_vkInvalidateMappedMemoryRanges)(VkDevice, uint32_t, const VkMappedMemoryRange *);
     VkResult (*p_vkLatencySleepNV)(VkDevice, VkSwapchainKHR, const VkLatencySleepInfoNV *);
     VkResult (*p_vkMapMemory)(VkDevice, VkDeviceMemory, VkDeviceSize, VkDeviceSize, VkMemoryMapFlags, void **);
@@ -573,13 +567,11 @@ struct vulkan_device_funcs
     void (*p_vkQueueInsertDebugUtilsLabelEXT)(VkQueue, const VkDebugUtilsLabelEXT *);
     void (*p_vkQueueNotifyOutOfBandNV)(VkQueue, const VkOutOfBandQueueTypeInfoNV *);
     VkResult (*p_vkQueuePresentKHR)(VkQueue, const VkPresentInfoKHR *);
-    VkResult (*p_vkQueueSetPerformanceConfigurationINTEL)(VkQueue, VkPerformanceConfigurationINTEL);
     VkResult (*p_vkQueueSubmit)(VkQueue, uint32_t, const VkSubmitInfo *, VkFence);
     VkResult (*p_vkQueueSubmit2)(VkQueue, uint32_t, const VkSubmitInfo2 *, VkFence);
     VkResult (*p_vkQueueSubmit2KHR)(VkQueue, uint32_t, const VkSubmitInfo2 *, VkFence);
     VkResult (*p_vkQueueWaitIdle)(VkQueue);
     VkResult (*p_vkReleaseCapturedPipelineDataKHR)(VkDevice, const VkReleaseCapturedPipelineDataInfoKHR *, const VkAllocationCallbacks *);
-    VkResult (*p_vkReleasePerformanceConfigurationINTEL)(VkDevice, VkPerformanceConfigurationINTEL);
     void (*p_vkReleaseProfilingLockKHR)(VkDevice);
     VkResult (*p_vkReleaseSwapchainImagesEXT)(VkDevice, const VkReleaseSwapchainImagesInfoEXT *);
     VkResult (*p_vkResetCommandBuffer)(VkCommandBuffer, VkCommandBufferResetFlags);
@@ -603,7 +595,6 @@ struct vulkan_device_funcs
     VkResult (*p_vkTransitionImageLayoutEXT)(VkDevice, uint32_t, const VkHostImageLayoutTransitionInfoEXT *);
     void (*p_vkTrimCommandPool)(VkDevice, VkCommandPool, VkCommandPoolTrimFlags);
     void (*p_vkTrimCommandPoolKHR)(VkDevice, VkCommandPool, VkCommandPoolTrimFlags);
-    void (*p_vkUninitializePerformanceApiINTEL)(VkDevice);
     void (*p_vkUnmapMemory)(VkDevice, VkDeviceMemory);
     VkResult (*p_vkUnmapMemory2KHR)(VkDevice, const VkMemoryUnmapInfoKHR *);
     void (*p_vkUpdateDescriptorSetWithTemplate)(VkDevice, VkDescriptorSet, VkDescriptorUpdateTemplate, const void *);
@@ -687,7 +678,6 @@ struct vulkan_instance_funcs
 #define ALL_VK_DEVICE_FUNCS() \
     USE_VK_FUNC(vkAcquireNextImage2KHR) \
     USE_VK_FUNC(vkAcquireNextImageKHR) \
-    USE_VK_FUNC(vkAcquirePerformanceConfigurationINTEL) \
     USE_VK_FUNC(vkAcquireProfilingLockKHR) \
     USE_VK_FUNC(vkAllocateCommandBuffers) \
     USE_VK_FUNC(vkAllocateDescriptorSets) \
@@ -899,9 +889,6 @@ struct vulkan_instance_funcs
     USE_VK_FUNC(vkCmdSetLogicOpEXT) \
     USE_VK_FUNC(vkCmdSetLogicOpEnableEXT) \
     USE_VK_FUNC(vkCmdSetPatchControlPointsEXT) \
-    USE_VK_FUNC(vkCmdSetPerformanceMarkerINTEL) \
-    USE_VK_FUNC(vkCmdSetPerformanceOverrideINTEL) \
-    USE_VK_FUNC(vkCmdSetPerformanceStreamMarkerINTEL) \
     USE_VK_FUNC(vkCmdSetPolygonModeEXT) \
     USE_VK_FUNC(vkCmdSetPrimitiveRestartEnable) \
     USE_VK_FUNC(vkCmdSetPrimitiveRestartEnableEXT) \
@@ -1135,7 +1122,6 @@ struct vulkan_instance_funcs
     USE_VK_FUNC(vkGetLatencyTimingsNV) \
     USE_VK_FUNC(vkGetMemoryHostPointerPropertiesEXT) \
     USE_VK_FUNC(vkGetMicromapBuildSizesEXT) \
-    USE_VK_FUNC(vkGetPerformanceParameterINTEL) \
     USE_VK_FUNC(vkGetPipelineBinaryDataKHR) \
     USE_VK_FUNC(vkGetPipelineCacheData) \
     USE_VK_FUNC(vkGetPipelineExecutableInternalRepresentationsKHR) \
@@ -1166,7 +1152,6 @@ struct vulkan_instance_funcs
     USE_VK_FUNC(vkGetSwapchainImagesKHR) \
     USE_VK_FUNC(vkGetValidationCacheDataEXT) \
     USE_VK_FUNC(vkGetVideoSessionMemoryRequirementsKHR) \
-    USE_VK_FUNC(vkInitializePerformanceApiINTEL) \
     USE_VK_FUNC(vkInvalidateMappedMemoryRanges) \
     USE_VK_FUNC(vkLatencySleepNV) \
     USE_VK_FUNC(vkMapMemory) \
@@ -1179,13 +1164,11 @@ struct vulkan_instance_funcs
     USE_VK_FUNC(vkQueueInsertDebugUtilsLabelEXT) \
     USE_VK_FUNC(vkQueueNotifyOutOfBandNV) \
     USE_VK_FUNC(vkQueuePresentKHR) \
-    USE_VK_FUNC(vkQueueSetPerformanceConfigurationINTEL) \
     USE_VK_FUNC(vkQueueSubmit) \
     USE_VK_FUNC(vkQueueSubmit2) \
     USE_VK_FUNC(vkQueueSubmit2KHR) \
     USE_VK_FUNC(vkQueueWaitIdle) \
     USE_VK_FUNC(vkReleaseCapturedPipelineDataKHR) \
-    USE_VK_FUNC(vkReleasePerformanceConfigurationINTEL) \
     USE_VK_FUNC(vkReleaseProfilingLockKHR) \
     USE_VK_FUNC(vkReleaseSwapchainImagesEXT) \
     USE_VK_FUNC(vkResetCommandBuffer) \
@@ -1209,7 +1192,6 @@ struct vulkan_instance_funcs
     USE_VK_FUNC(vkTransitionImageLayoutEXT) \
     USE_VK_FUNC(vkTrimCommandPool) \
     USE_VK_FUNC(vkTrimCommandPoolKHR) \
-    USE_VK_FUNC(vkUninitializePerformanceApiINTEL) \
     USE_VK_FUNC(vkUnmapMemory) \
     USE_VK_FUNC(vkUnmapMemory2KHR) \
     USE_VK_FUNC(vkUpdateDescriptorSetWithTemplate) \
