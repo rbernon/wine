7b6c6a21f374e8e8e64728d9faeee13de9114820
Rémi Bernon <rbernon@codeweavers.com>
2024-09-26 10:36:55 +0200

    d3d8/tests: Flush events after window creation and before changing display settings.
    Since 2b32c2859166e5542012b517cdef4699d8bb322c we update the driver
    window state on display mode change, which creates more opportunities
    of ConfigureNotify race conditions if there are some X11 events pending
    while display settings is updated.

--- dlls/d3d8/tests/device.c
+++ dlls/d3d8/tests/device.c
@@ -10928,6 +10928,8 @@ static void test_window_position(void)
         ok(ret, "Adapter %u: GetMonitorInfoW failed, error %#lx.\n", adapter_idx, GetLastError());
 
         window = create_window();
+        flush_events();
+
         device_desc.adapter_ordinal = adapter_idx;
         device_desc.device_window = window;
         device_desc.width = monitor_info.rcMonitor.right - monitor_info.rcMonitor.left;
