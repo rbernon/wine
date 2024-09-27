b06b210fc860a0bb64743fbec5e996470850ee58
Rémi Bernon <rbernon@codeweavers.com>
2024-09-27 15:28:24 +0200

    flush_events

--- dlls/user32/tests/monitor.c
+++ dlls/user32/tests/monitor.c
@@ -93,6 +93,10 @@ static void flush_events(void)
     DWORD time;
     MSG msg;
 
+    while (PeekMessageA(&msg, 0, 0, 0, PM_REMOVE))
+        DispatchMessageA(&msg);
+    return;
+
     time = GetTickCount() + diff;
     while (diff > 0)
     {
