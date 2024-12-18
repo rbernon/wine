bb617e5a86d122d1e0a12517e0e755493b19e582
Rémi Bernon <rbernon@codeweavers.com>
2024-12-18 16:18:39 +0100

    win32u: Update vulkan surfaces when updating the window state.

--- dlls/win32u/window.c
+++ dlls/win32u/window.c
@@ -4600,6 +4600,7 @@ void update_window_state( HWND hwnd )
     if (surface) window_surface_release( surface );
 
     set_thread_dpi_awareness_context( context );
+    vulkan_update_surfaces( hwnd );
 }
 
 /***********************************************************************
