d2fa814b2263174c1f25c13ea07abd9e6867d717
Rémi Bernon <rbernon@codeweavers.com>
2024-12-19 10:16:27 +0100

    winex11: Create DCs with the onscreen client window when possible.

--- dlls/winex11.drv/window.c
+++ dlls/winex11.drv/window.c
@@ -2763,7 +2763,17 @@ void X11DRV_GetDC( HDC hdc, HWND hwnd, HWND top, const RECT *win_rect,
 
     if ((data = get_win_data( top )))
     {
-        escape.drawable = data->whole_window;
+        if (!(escape.drawable = data->client_window) || (flags & DCX_WINDOW))
+        {
+            if (data->client_window) WARN( "window %p/%lx has an onscreen client window\n", data->hwnd, data->whole_window );
+            escape.drawable = data->whole_window;
+        }
+        else
+        {
+            OffsetRect( &escape.dc_rect, data->rects.client.left - data->rects.window.left,
+                        data->rects.client.top - data->rects.window.top );
+        }
+
         escape.visual = data->vis;
         /* special case: when repainting the root window, clip out top-level windows */
         if (top == hwnd && data->whole_window == root_window) escape.mode = ClipByChildren;
