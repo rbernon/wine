521f3ac414cac095c36ab6f1b070ab6ec7852e46
Rémi Bernon <rbernon@codeweavers.com>
2025-02-15 15:49:01 +0100

    Revert "Merge branch 'wip/just-use-zink'"
    This reverts commit 5999d0c282b5675e112a7ef2c4fb265692af32e1, reversing
    changes made to 26703b7e5ad2f256e0d76fdacbff02aa176270e2.

--- dlls/opengl32/unixlib.h
+++ dlls/opengl32/unixlib.h
@@ -3,7 +3,6 @@
 #ifndef __WINE_OPENGL32_UNIXLIB_H
 #define __WINE_OPENGL32_UNIXLIB_H
 
-#ifndef ENABLE_MESA
 #include <stdarg.h>
 #include <stddef.h>
 
@@ -28401,5 +28400,4 @@ struct gl_debug_message_callback_params
 
 #define UNIX_CALL( func, params ) WINE_UNIX_CALL( unix_ ## func, params )
 
-#endif /* ENABLE_MESA */
 #endif /* __WINE_OPENGL32_UNIXLIB_H */
